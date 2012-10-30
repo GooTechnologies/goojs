83d1e142fe67898ef57a4e97b297869a994021ca
Joel <joel@gooengine.com>
2012-10-30 18:11:19 +0100

    Updates to vector classes.

--- TestDynamic/WebContent/goo/math/Vector.js
+++ TestDynamic/WebContent/goo/math/Vector.js
@@ -1,99 +1,247 @@
-define(function() {
+define([], function() {
 	"use strict";
 
 	/**
-	 * Creates a new N-dimensional vector.
-	 * 
+	 * @class N-dimensional vector.
 	 * @name Vector
-	 * @class N-dimensional vector class
-	 * @param size {Integer} Number of vector components
-	 * @property data {Float[]} data Storage for vector components
+	 * @property {Float32Array} data Storage for the vector components.
+	 * @property {Integer} tos Top of stack for putting components.
+	 * @constructor
+	 * @description Creates a new N-dimensional vector.
+	 * @param {Integer} size Number of vector components.
 	 */
 
 	function Vector(size) {
 		this.data = new Float32Array(size || 0);
+		this.tos = 0;
 	}
 
 	/**
-	 * Add two N-dimensional vectors and store the result in a new vector.
-	 * 
-	 * @param lhs {@link Vector} Left-hand side
-	 * @param rhs {@link Vector} Right-hand side
-	 * @param target {@link Vector} Target vector (optional)
+	 * @static
+	 * @description Adds two N-dimensional vectors and stores the result in a separate vector. The resulting vector will
+	 *              have a size equal to that of the left-hand vector.
+	 * @param {Vector} lhs Vector on the left-hand side.
+	 * @param {Vector} rhs Vector on the right-hand side.
+	 * @param {Vector} target Target vector for storage. (optional)
+	 * @returns {Vector} Resulting vector.
 	 */
 
 	Vector.add = function(lhs, rhs, target) {
-		var size = Math.min(lhs.data.length, rhs.data.length);
-
-		if (!target) {
-			target = new Vector(size);
+		if (!target || target.data.length != lhs.data.length) {
+			target = new Vector(lhs.data.length);
 		}
 
-		for ( var i = 0; i < size; i++) {
+		var i = 0;
+
+		for (; i < Math.min(lhs.data.length, rhs.data.length); i++) {
 			target.data[i] = lhs.data[i] + rhs.data[i];
 		}
 
+		for (; i < lhs.data.length; i++) {
+			target.data[i] = lhs.data[i];
+		}
+
 		return target;
 	};
 
 	/**
-	 * Subtract two N-dimensional vectors and store the result in a new vector.
-	 * 
-	 * @param lhs {@link Vector} Left-hand side
-	 * @param rhs {@link Vector} Right-hand side
-	 * @param target {@link Vector} Target vector (optional)
+	 * @static
+	 * @description Subtracts two N-dimensional vectors and stores the result in a separate vector. The resulting vector
+	 *              will have a size equal to that of the left-hand vector.
+	 * @param {Vector} lhs Vector on the left-hand side.
+	 * @param {Vector} rhs Vector on the right-hand side.
+	 * @param {Vector} target Target vector for storage. (optional)
+	 * @returns {Vector} Resulting vector.
 	 */
 
 	Vector.sub = function(lhs, rhs, target) {
-		var size = Math.min(lhs.data.length, rhs.data.length);
-
-		if (!target) {
-			target = new Vector(size);
+		if (!target || target.data.length != lhs.data.length) {
+			target = new Vector(lhs.data.length);
 		}
 
-		for ( var i = 0; i < size; i++) {
+		var i = 0;
+
+		for (; i < Math.min(lhs.data.length, rhs.data.length); i++) {
 			target.data[i] = lhs.data[i] - rhs.data[i];
+		}
+
+		for (; i < lhs.data.length; i++) {
+			target.data[i] = lhs.data[i];
 		}
 
 		return target;
 	};
 
 	/**
-	 * Add an N-dimensional vector and store the result locally.
-	 * 
-	 * @param rhs {@link Vector} Right-hand side
+	 * @static
+	 * @description Multiplies two N-dimensional vectors and stores the result in a separate vector. The resulting
+	 *              vector will have a size equal to that of the left-hand vector.
+	 * @param {Vector} lhs Vector on the left-hand side.
+	 * @param {Vector} rhs Vector on the right-hand side.
+	 * @param {Vector} target Target vector for storage. (optional)
+	 * @returns {Vector} Resulting vector.
+	 */
+
+	Vector.mul = function(lhs, rhs, target) {
+		if (!target || target.data.length != lhs.data.length) {
+			target = new Vector(lhs.data.length);
+		}
+
+		var i = 0;
+
+		for (; i < Math.min(lhs.data.length, rhs.data.length); i++) {
+			target.data[i] = lhs.data[i] * rhs.data[i];
+		}
+
+		for (; i < lhs.data.length; i++) {
+			target.data[i] = lhs.data[i];
+		}
+
+		return target;
+	};
+
+	/**
+	 * @static
+	 * @description Divides two N-dimensional vectors and stores the result in a separate vector. The resulting vector
+	 *              will have a size equal to that of the left-hand vector. For all components in the right-hand vector
+	 *              equal to zero, the corresponding component in the resulting vector will be equal to that of the
+	 *              left-hand vector.
+	 * @param {Vector} lhs Vector on the left-hand side.
+	 * @param {Vector} rhs Vector on the right-hand side.
+	 * @param {Vector} target Target vector for storage. (optional)
+	 * @returns {Vector} Resulting vector.
+	 */
+
+	Vector.div = function(lhs, rhs, target) {
+		if (!target || target.data.length != lhs.data.length) {
+			target = new Vector(lhs.data.length);
+		}
+
+		var i = 0;
+
+		for (; i < Math.min(lhs.data.length, rhs.data.length); i++) {
+			if (rhs.data[i] < 0.0 || rhs.data[i] > 0.0) {
+				target.data[i] = lhs.data[i] / rhs.data[i];
+			} else {
+				target.data[i] = lhs.data[i];
+			}
+		}
+
+		for (; i < lhs.data.length; i++) {
+			target.data[i] = lhs.data[i];
+		}
+
+		return target;
+	};
+
+	/**
+	 * @static
+	 * @description Scales an N-dimensional vector with a scalar and stores the result in a separate vector.
+	 * @param {Vector} lhs Vector on the left-hand side.
+	 * @param {Float} rhs Scalar on the right-hand side.
+	 * @param {Vector} target Target vector for storage. (optional)
+	 * @returns {Vector} Resulting vector.
+	 */
+
+	Vector.scale = function(lhs, rhs, target) {
+		if (!target || target.data.length != lhs.data.length) {
+			target = new Vector(lhs.data.length);
+		}
+
+		for ( var i = 0; i < lhs.data.length; i++) {
+			target.data[i] = lhs.data[i] * rhs;
+		}
+
+		return target;
+	};
+
+	/**
+	 * @description Adds with an N-dimensional vector and stores the result locally.
+	 * @param {Vector} rhs Vector on the right-hand side.
+	 * @returns {Vector} Self for chaining.
 	 */
 
 	Vector.prototype.add = function(rhs) {
-		var size = Math.min(this.data.length, rhs.data.length);
-
-		for ( var i = 0; i < size; i++) {
-			this.data[i] += b.data[i];
-		}
-
-		return this;
+		return Vector.add(this, rhs, this);
 	};
 
 	/**
-	 * Subtract an N-dimensional vector and store the result locally.
-	 * 
-	 * @param rhs {@link Vector} Right-hand side
+	 * @description Subtracts with an N-dimensional vector and stores the result locally.
+	 * @param {Vector} rhs Vector on the right-hand side.
+	 * @returns {Vector} Self for chaining.
 	 */
 
 	Vector.prototype.sub = function(rhs) {
-		var size = Math.min(this.data.length, rhs.data.length);
+		return Vector.sub(this, rhs, this);
+	};
 
-		for ( var i = 0; i < size; i++) {
-			this.data[i] -= b.data[i];
+	/**
+	 * @description Multiplies by an N-dimensional vector and stores the result locally.
+	 * @param {Vector} rhs Vector on the right-hand side.
+	 * @returns {Vector} Self for chaining.
+	 */
+
+	Vector.prototype.mul = function(rhs) {
+		return Vector.mul(this, rhs, this);
+	};
+
+	/**
+	 * @description Divides by an N-dimensional vector and stores the result locally.
+	 * @param {Vector} rhs Vector on the right-hand side.
+	 * @returns {Vector} Self for chaining.
+	 */
+
+	Vector.prototype.div = function(rhs) {
+		return Vector.div(this, rhs, this);
+	};
+
+	/**
+	 * @description Scales by a scalar and stores the result locally.
+	 * @param {Float} rhs Scalar on the right-hand side.
+	 * @returns {Vector} Self for chaining.
+	 */
+
+	Vector.prototype.scale = function(rhs) {
+		return Vector.scale(this, rhs, this);
+	};
+
+	/**
+	 * @description Copies all components from another N-dimensional vector and resizes itself if the sizes do not
+	 *              match.
+	 * @param {Vector} source Source vector.
+	 * @returns {Vector} Self for chaining.
+	 */
+
+	Vector.prototype.copy = function(source) {
+		if (this.data.length != source.data.length) {
+			this.data = new Float32Array(source.data.length);
+		}
+
+		for ( var i = 0; i < this.data.length; i++) {
+			this.data[i] = source.data[i];
+		}
+
+		this.tos = this.data.length;
+
+		return this;
+	};
+
+	/**
+	 * @description Inverts all components of the N-dimensional vector.
+	 * @returns {Vector} Self for chaining.
+	 */
+
+	Vector.prototype.invert = function() {
+		for ( var i = 0; i < this.data.length; i++) {
+			this.data[i] = 0.0 - this.data[i];
 		}
 
 		return this;
 	};
 
 	/**
-	 * Subtract an N-dimensional vector and store the result locally.
-	 * 
-	 * @param rhs {@link Vector} Right-hand side
+	 * @description Computes the dot product of the N-dimensional vector.
+	 * @returns {Float} Dot product.
 	 */
 
 	Vector.prototype.dot = function() {
@@ -106,8 +254,112 @@ define(function() {
 		return result;
 	};
 
+	/**
+	 * @description Computes the length (euclidian norm) of the N-dimensional vector.
+	 * @returns {Float} Length.
+	 */
+
 	Vector.prototype.length = function() {
 		return Math.sqrt(this.dot());
+	};
+
+	/**
+	 * @description Normalizes the vector to unit length.
+	 * @returns {Vector} Self for chaining.
+	 */
+
+	Vector.prototype.normalize = function() {
+		var l = this.length();
+
+		if (l > 0.0) {
+			l = 1.0 / l;
+
+			for ( var i = 0; i < this.data.length; i++) {
+				this.data[i] *= l;
+			}
+		}
+
+		return this;
+	};
+
+	/**
+	 * @description Gets a component from the vector.
+	 * @param {Integer} index Component index.
+	 * @throws {IndexExceedsDimensions} If an illegal index is accessed.
+	 * @returns {Float} Component value.
+	 */
+
+	Vector.prototype.get = function(index) {
+		if (index >= 0 && index < this.data.length) {
+			return this.data[index];
+		} else {
+			throw {
+				name : "IndexExceedsDimensions",
+			};
+		}
+	};
+
+	/**
+	 * @description Sets a component in the vector.
+	 * @param {Integer} index Component index.
+	 * @param {Float} value Component value.
+	 * @throws {IndexExceedsDimensions} If an illegal index is accessed.
+	 * @returns {Vector} Self for chaining.
+	 */
+
+	Vector.prototype.set = function(index, value) {
+		if (index >= 0 && index < this.data.length) {
+			this.data[index] = value;
+		} else {
+			throw {
+				name : "IndexExceedsDimensions",
+			};
+		}
+
+		return this;
+	};
+
+	/**
+	 * @description Puts a component into the vector.
+	 * @param {Float} value Component value.
+	 * @throws {IndexExceedsDimensions} If an illegal index is accessed.
+	 * @returns {Vector} Self for chaining.
+	 */
+
+	Vector.prototype.put = function(value) {
+		if (this.tos < this.data.length) {
+			this.data[this.tos++] = value;
+		} else {
+			throw {
+				name : "IndexExceedsDimensions",
+			};
+		}
+
+		return this;
+	};
+
+	/**
+	 * @description Converts the N-dimensional vector to a string.
+	 * @returns {String} String of values.
+	 */
+
+	Vector.prototype.toString = function() {
+		var string = "";
+		var i = 0;
+
+		string += "[";
+
+		for (; i < this.data.length - 1; i++) {
+			string += " " + this.data[i] + ",";
+		}
+
+		for (; i < this.data.length; i++) {
+			string += " " + this.data[i] + " ";
+		}
+
+		string += "]";
+
+		return string;
 	};
 
 	return Vector;
--- TestDynamic/WebContent/math.js
+++ TestDynamic/WebContent/math.js
@@ -3,14 +3,45 @@
 require(["goo/math/Vector", "goo/math/Vector4"], function(Vector, Vector4) {
 
 	function init() {
-		var a = new Vector4(1.0, 1.0, 1.0, 1.0);
-		var b = new Vector4(2.0, 2.0, 2.0, 2.0);
+		testVector();
+		testVector4();
+	}
 
-		console.log(a.add(b)); // a' is now a + b
-		console.log(Vector4.add(a, b)); // new vector of a' + b = a + 2b
-		console.log(a); // Should print {3, 3, 3, 3}
-		console.log(b); // Should print {2, 2, 2, 2}
-		console.log(a.dot()); // Should print 36
+	function testVector() {
+		var a = new Vector(2).put(3.0).put(4.0);
+		console.log(Vector.add(a, a, a) + " expected [ 6.0, 8.0 ]");
+
+		var b = new Vector(2).put(3.0).put(4.0);
+		console.log(Vector.sub(b, b, b) + " expected [ 0.0, 0.0 ]");
+
+		var c = new Vector(2).put(3.0).put(4.0);
+		console.log(Vector.mul(c, c, c) + " expected [ 9.0, 16.0 ]");
+
+		var d = new Vector(2).put(3.0).put(4.0);
+		console.log(Vector.div(d, d, d) + " expected [ 1.0, 1.0 ]");
+
+		var e = new Vector(2).put(3.0).put(4.0);
+		console.log(Vector.scale(e, 2.0, e) + " expected [ 6.0, 8.0 ]");
+
+		var f = new Vector(2).set(0, 3.0).set(1, 4.0);
+		console.log(f + " expected [ 3.0, 4.0 ]");
+		console.log(f.invert() + " expected [ -3.0, -4.0 ]");
+
+		var g = new Vector(2).put(3.0).put(4.0);
+		console.log(g + " expected [ 3.0, 4.0 ]");
+		console.log(g.dot() + " expected 25.0");
+		console.log(g.length() + " expected 5.0");
+		console.log(g.get(0) + " expected 3.0");
+		console.log(g.get(1) + " expected 4.0");
+		console.log(g.normalize() + " expected [ 0.6, 0.8 ]");
+
+		var h = new Vector(2).copy(f);
+		console.log(h + " expected [ -3.0, -4.0 ]");
+	}
+
+	function testVector4() {
+		var a = new Vector4(3.0, 4.0, 0.0, 0.0);
+		console.log(a + " expected [ 3.0, 4.0, 0.0, 0.0 ]");
 	}
 
 	init();
