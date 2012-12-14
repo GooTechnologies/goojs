98d460e1e88a45c8bf8538e47662a1c0ee306e8a
Marios Nikolaou <marios.nikolaou@gmail.com>
2012-12-14 12:10:53 +0100

    Fix math tests to testacular main require.js file
    rherlitz/GooEngine#52

--- test/main-test.js
+++ test/main-test.js
@@ -6,7 +6,18 @@ require.config({
     waitSeconds: 5
   });
 require([
-    'test/mathTest',
+    // Require does not allow us to add dependencies with wildcards...
+    'test/math/MathUtils',
+    'test/math/Matrix',
+    'test/math/Matrix2x2',
+    'test/math/Matrix3x3',
+    'test/math/Matrix4x4',
+    'test/math/Quaternion',
+    'test/math/Transform',
+    'test/math/Vector',
+    'test/math/Vector2',
+    'test/math/Vector3',
+    'test/math/Vector4',
     'test/shapesTest',
     'test/entities/entitiesTest'
 ],
