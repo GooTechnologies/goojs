16e119eb912be499d21157085ba9824ca2c1f084
rherlitz <rikard@gootechnologies.com>
2013-01-02 11:53:52 +0100

    Check for maximum WebGL vertex count.

--- src/goo/renderer/BufferUtils.js
+++ src/goo/renderer/BufferUtils.js
@@ -1,17 +1,19 @@
-define(function () {
+define(function() {
 	"use strict";
 
 	function BufferUtils() {
 	}
 
-	BufferUtils.createIntBuffer = function (indexCount, vertexCount) {
+	BufferUtils.createIntBuffer = function(indexCount, vertexCount) {
 		var indices;
 		if (vertexCount < 256) { // 2^8
 			indices = new Int8Array(indexCount);
 		} else if (vertexCount < 65536) { // 2^16
 			indices = new Int16Array(indexCount);
 		} else { // 2^32
-			indices = new Int32Array(indexCount);
+			// XXX: Currently not allowed in WebGL. Max is 16 bits.
+			throw new Error("Maximum number of vertices is 65536. Got: " + vertexCount);
+			// indices = new Int32Array(indexCount);
 		}
 		return indices;
 	};
