8ffe7565cc0d13794d6351f8cb911d79bfb217b7
rherlitz <rikard@gootechnologies.com>
2013-01-29 00:03:53 +0100

    Start of require optimization tests

--- build/javabuild.js
+++ build/javabuild.js
@@ -1,35 +1,14 @@
-// var requirejs = require('requirejs');
-
-// var config = {
 ({
-	baseUrl : '../WebContent',
-	name : 'nes',
-	out : 'main-built-java.js',
+	baseUrl : '../examples',
+	paths: {
+        goo: '../src/goo',
+		almond: '../build/almond'
+    },	
+	name: 'almond',
+    include: ['start'],
+    insertRequire: ['start'],
+	out : 'extracted.js',
 	useStrict : true,
-	optimize : 'closure',
-	uglify : {
-		toplevel : false,
-		ascii_only : false
-	// beautify: true,
-	// max_line_length: 120,
-
-	// Custom value supported by r.js but done differently
-	// in uglifyjs directly:
-	// Skip the processor.ast_mangle() part of the uglify call (r.js 2.0.5+)
-	// no_mangle: true
-	},
-	closure : {
-		CompilerOptions : {},
-		// CompilationLevel : 'SIMPLE_OPTIMIZATIONS',
-		CompilationLevel : 'ADVANCED_OPTIMIZATIONS',
-		loggingLevel : 'WARNING'
-	}
-// };
+	optimize : 'none',
+	wrap: true
 })
-
-// requirejs.optimize(config, function(buildResponse) {
-// buildResponse is just a text output of the modules
-// included. Load the built file for the contents.
-// Use config.out to get the optimized file contents.
-// var contents = fs.readFileSync(config.out, 'utf8');
-// });
