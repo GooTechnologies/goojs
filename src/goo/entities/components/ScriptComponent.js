b1a7d7b440a94d3c0747f056664747c9ab1c3296
Renanse <renanse@gmail.com>
2013-02-04 10:42:33 -0600

    Just expanded ScriptComponent to allow for multiple scripts.

--- src/goo/entities/components/ScriptComponent.js
+++ src/goo/entities/components/ScriptComponent.js
@@ -1,23 +1,31 @@
 define(['goo/entities/components/Component'],
-	/** @lends ScriptComponent */
-	function (Component) {
+/** @lends ScriptComponent */
+function(Component) {
 	"use strict";
 
 	/**
 	 * @class Contains scripts to be executed each frame when set on an active entity
 	 * @param {JS} script Script to contain in this script component
 	 */
-	function ScriptComponent(script) {
+	function ScriptComponent(scripts) {
 		this.type = 'ScriptComponent';
 
-		this.script = script;
+		if (scripts instanceof Array) {
+			this.scripts = scripts;
+		} else {
+			this.scripts = [scripts];
+		}
 	}
 
 	ScriptComponent.prototype = Object.create(Component.prototype);
 
-	ScriptComponent.prototype.run = function (entity) {
-		if (this.script && this.script.run) {
-			this.script.run(entity);
+	ScriptComponent.prototype.run = function(entity) {
+		var script;
+		for ( var i = 0, max = this.scripts.length; i < max; i++) {
+			script = this.scripts[i];
+			if (script && script.run) {
+				script.run(entity);
+			}
 		}
 	};
 
