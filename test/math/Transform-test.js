4a92d0d70d51e1cb3039a9b8f7e3c1ca834c0f0d
Martin Vilcans <martin@librador.com>
2013-02-05 21:25:29 +0100

    Some tests for Transform.invert
    Story #58

--- test/math/Transform-test.js
+++ test/math/Transform-test.js
@@ -7,12 +7,27 @@ define([
 ) {
 	'use strict';
 
+	/**
+	 * Checks whether Transform.invert works on a test vector.
+	 */
+	function checkInversion (transform) {
+		var vec1 = new Vector3(100, 200, 300);
+		var vec2 = new Vector3();
+		var vec3 = new Vector3();
+		var inverted = transform.invert();
+		transform.applyForward(vec1, vec2);
+		inverted.applyForward(vec2, vec3);
+		expect(vec3).toBeEqualToVector(vec1);
+	}
+
+
 	describe('Transform', function() {
-		var t, v1, v2;
+		var t, v1, v2, v3;
 		beforeEach(function() {
 			t = new Transform();
 			v1 = new Vector3(10, 20, 30);
 			v2 = new Vector3(0, 0, 0);
+			v3 = new Vector3(0, 0, 0);
 
 			this.addMatchers({
 				toBeEqualToVector: function(expected) {
@@ -38,6 +53,28 @@ define([
 			t.update();
 			t.applyForward(v1, v2);
 			expect(v2).toBeEqualToVector(new Vector3(10 * 2, 20 * 3, 30 * 4));
+		});
+		it('can be rotated around X axis', function() {
+			t.rotation.x = Math.PI / 2;
+			t.update();
+			t.applyForward(v1, v2);
+			expect(v2).toBeEqualToVector(new Vector3(10, -30, 20));
+		});
+		it('can be inverted if identity', function() {
+			checkInversion(t);
+		});
+		it('can be inverted if scaled', function() {
+			t.scale.x = 2;
+			t.scale.y = 3;
+			t.scale.z = 4;
+			t.update();
+			checkInversion(t);
+		});
+		it('can be inverted if rotated', function() {
+			var inverted = new Transform();
+			t.rotation.x = .2;
+			t.update();
+			checkInversion(t);
 		});
 	});
 
