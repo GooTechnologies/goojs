115623667d8d0fbd56f4466aa9ff3f51f35c83b9
Martin Vilcans <martin@librador.com>
2013-02-05 15:57:13 +0100

    A couple of tests for Transform.
    Story #58

--- test/math/Transform-test.js
+++ test/math/Transform-test.js
@@ -1,3 +1,44 @@
-define(["goo/math/Transform"], function(Transform) {
-	"use strict";
+define([
+	'goo/math/Transform',
+	'goo/math/Vector3'
+], function(
+	Transform,
+	Vector3
+) {
+	'use strict';
+
+	describe('Transform', function() {
+		var t, v1, v2;
+		beforeEach(function() {
+			t = new Transform();
+			v1 = new Vector3(10, 20, 30);
+			v2 = new Vector3(0, 0, 0);
+
+			this.addMatchers({
+				toBeEqualToVector: function(expected) {
+					var actual = this.actual;
+					var notText = this.isNot ? ' not' : '';
+					this.message = function () {
+						return 'Expected ' + actual +
+							notText + ' to be equal to vector ' + expected;
+					};
+					return actual.equals(expected);
+				}
+			});
+		});
+
+		it('is identity by default', function() {
+			t.applyForward(v1, v2);
+			expect(v2).toBeEqualToVector(v1);
+		});
+		it('can be scaled', function() {
+			t.scale.x = 2;
+			t.scale.y = 3;
+			t.scale.z = 4;
+			t.update();
+			t.applyForward(v1, v2);
+			expect(v2).toBeEqualToVector(new Vector3(10 * 2, 20 * 3, 30 * 4));
+		});
+	});
+
 });
