a3072f1ebf5e5e4554a626b9a7d7acb7c77d17ad
Marcus Stenbeck <marcus.stenbeck@gmail.com>
2013-02-25 11:49:02 +0100

    Added basePath to require in testacular main runner.

--- test/testacular-main.js
+++ test/testacular-main.js
@@ -1,12 +1,15 @@
 // Main module for testacular tests
 require.config({
-    paths: {
-        "goo": "src/goo",
-        "test": "test",
-        "goo/lib": "lib"
-    },
-    waitSeconds: 5
-  });
+	baseUrl: '/base',
+	paths: {
+		"goo": "src/goo",
+		"test": "test",
+		"goo/lib": "lib"
+	},
+	waitSeconds: 5
+});
+
+
 require(['test/all-tests'], function() {
-  window.__testacular__.start();
+	window.__testacular__.start();
 });
