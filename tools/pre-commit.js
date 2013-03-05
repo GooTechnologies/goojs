3d952990dbb5fc606785ca3b34a578d14e0ba6c9
Joel Ek <joel@gootechnologies.com>
2013-03-05 17:29:47 +0100

    Added pre-commit script. Activate by creating a file named pre-commit in .git/hooks. It should contain two rows: #!/bin/sh and exec < /dev/tty node tools/pre-commit.js . Deliberately introduced two errors in order to test.

---
+++ tools/pre-commit.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var exec = require('child_process').exec;
+
+exec('git status', function (error, stdout, stderr) {
+	var lines = stdout.split('\n');
+
+	if (lines.length > 0) {
+		var files = [];
+		var expression = /(new file|modified)\:\s*(\S+\.js)\s*/;
+
+		lines.forEach(function (line) {
+			var match = expression.exec(line);
+
+			if (match) {
+				files[files.length] = match[2];
+			}
+		});
+
+		var child = exec('jshint --reporter=tools/jshint-reporter.js ' + files.join(' '));
+
+		child.stdout.on('data', function (data) {
+			process.stdout.write(data);
+		});
+
+		child.on('exit', function (code) {
+			if (code !== 0) {
+				process.stdout.write('One or more of the files you are trying to commit have style errors (see the above output). Are you sure you want to commit? [y/n] ');
+
+				var stdin = process.stdin;
+
+				stdin.setRawMode(true);
+				stdin.resume();
+				stdin.setEncoding('utf8');
+
+				stdin.on('data', function (key) {
+					if (key === 'y' || key === 'Y') {
+						process.stdout.write(key);
+						process.exit(0);
+					} else if (key === 'n' || key === 'N') {
+						process.stdout.write(key);
+						process.exit(1);
+					}
+				});
+			} else {
+				process.exit(0);
+			}
+		});
+	}
+});
