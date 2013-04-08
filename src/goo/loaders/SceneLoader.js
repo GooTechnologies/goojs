0a5bb6d94f247645556c62b00b96baa439b6ea2e
rherlitz <rikard@gootechnologies.com>
2013-04-08 17:52:11 +0200

    Hacked in access to globals in scene bundles. Better formatting

--- src/goo/loaders/SceneLoader.js
+++ src/goo/loaders/SceneLoader.js
@@ -1,18 +1,16 @@
 /* jshint bitwise: false */
 define([
-		'goo/loaders/Loader',
-		'goo/loaders/EntityLoader',
-
-		'goo/lib/rsvp.amd'
-	],
+	'goo/loaders/Loader',
+	'goo/loaders/EntityLoader',
+	'goo/lib/rsvp.amd'
+],
 /** @lends SceneLoader */
-function(
-		Loader,
-		EntityLoader,
-		RSVP
-	) {
+function (
+	Loader,
+	EntityLoader,
+	RSVP
+) {
 	"use strict";
-
 
 	/**
 	 * Utility class for loading scenes into a World.
@@ -22,15 +20,15 @@ function(
 	 * @param {Loader} parameters.loader
 	 */
 	function SceneLoader(parameters) {
-		if(typeof parameters === "undefined" || parameters === null) {
+		if (typeof parameters === "undefined" || parameters === null) {
 			throw new Error('SceneLoader(): Argument `parameters` was undefined/null');
 		}
 
-		if(typeof parameters.loader === "undefined" || !(parameters.loader instanceof Loader) || parameters.loader === null) {
+		if (typeof parameters.loader === "undefined" || !(parameters.loader instanceof Loader) || parameters.loader === null) {
 			throw new Error('SceneLoader(): Argument `parameters.loader` was invalid/undefined/null');
 		}
 
-		if(typeof parameters.world === "undefined" || parameters.world === null) {
+		if (typeof parameters.world === "undefined" || parameters.world === null) {
 			throw new Error('SceneLoader(): Argument `parameters.world` was undefined/null');
 		}
 
@@ -44,49 +42,50 @@ function(
 	 * @param {string} scenePath Relative path to the scene.
 	 * @return {Promise} The promise is resolved with the target World object.
 	 */
-	SceneLoader.prototype.load = function(scenePath) {
+	SceneLoader.prototype.load = function (scenePath) {
 		var that = this;
-		return this._loader.load(scenePath, function(data) {
+		return this._loader.load(scenePath, function (data) {
 			return that._parse(data, scenePath);
 		});
 	};
 
-	SceneLoader.prototype._parse = function(sceneSource, scenePath) {
+	SceneLoader.prototype._parse = function (sceneSource, scenePath) {
 		var promises = [];
 		// If we got files, then let's do stuff with the files!
-		if(sceneSource && sceneSource.files && sceneSource.files.length)
-		{
+		if (sceneSource && sceneSource.files && sceneSource.files.length) {
 			var entityLoader = new EntityLoader({
 				world: this._world,
 				loader: this._loader
 			});
 
-			for(var i in sceneSource.files) {
+			for (var i in sceneSource.files) {
 				// Check if they're entities
 				var fileName = sceneSource.files[i];
 
-				if(/\.ent$/.test(fileName)) {
+				if (/\.ent$/.test(fileName)) {
 					var p = entityLoader.load(fileName);
 					promises.push(p);
 				}
 			}
 		}
 
-		if(promises.length === 0) {
+		if (promises.length === 0) {
 			var p = new RSVP.Promise();
 			p.reject('Can\'t find anything to load at ' + scenePath);
 			return p;
 		}
 
+		this.globals = sceneSource.globals;
+
 		// Create a promise that resolves when all promise-objects
 		return RSVP.all(promises)
-		.then(function(entities) {
-			return entities;
-		});
+			.then(function (entities) {
+				return entities;
+			});
 	};
 
-	SceneLoader.prototype._buildWorld = function(entities) {
-		for(var i in entities) {
+	SceneLoader.prototype._buildWorld = function (entities) {
+		for (var i in entities) {
 			entities[i].addToWorld();
 		}
 		this._world.process();
