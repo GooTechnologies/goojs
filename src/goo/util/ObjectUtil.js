844d9b1e6a9a573160ab5df9fbd0d0b9000d7858
Martin Berg <martin.berg@gooengine.com>
2013-06-12 17:29:00 +0200

    Fixing #184 according to review comments

--- src/goo/util/ObjectUtil.js
+++ src/goo/util/ObjectUtil.js
@@ -1,13 +1,32 @@
-
-// Underscore.js cutout
+/** 
+ * Everything we need from underscore.js. Convenience stuff, copied straight off. 
+ * For documentation, see http://underscorejs.org. Gotta love open source.
+ */
 define([], function() {		
 	var _ = {}
 	
-	_.clone = function(obj) {
-    if (!_.isObject(obj)) return obj;
-    return _.isArray(obj) ? obj.slice() : _.extend({}, obj);
-  }
 	
+	// Save bytes in the minified (but not gzipped) version:
+  var ArrayProto = Array.prototype, ObjProto = Object.prototype, FuncProto = Function.prototype;
+
+  // Create quick reference variables for speed access to core prototypes.
+  var push             = ArrayProto.push,
+      slice            = ArrayProto.slice,
+      concat           = ArrayProto.concat,
+      toString         = ObjProto.toString,
+      hasOwnProperty   = ObjProto.hasOwnProperty;
+
+	// Maybe there is an Array.isArray
+	var nativeIsArray 		= Array.isArray,
+			nativeKeys         = Object.keys,
+    	nativeForEach     = ArrayProto.forEach;
+ 
+	
+	
+	_.has = function(obj, key) {
+    return hasOwnProperty.call(obj, key);
+  };
+  
 	_.defaults = function(obj) {
     each(slice.call(arguments, 1), function(source) {
       if (source) {
@@ -29,6 +48,52 @@ define([], function() {
     });
     return obj;
   };
+	
+  _.isObject = function(obj) {
+    return obj === Object(obj);
+  };
+	
+	// Create a (shallow-cloned) duplicate of an object.
+	_.clone = function(obj) {
+    if (!_.isObject(obj)) return obj;
+    return _.isArray(obj) ? obj.slice() : _.extend({}, obj);
+  };
+  
+  _.keys = nativeKeys || function(obj) {
+    if (obj !== Object(obj)) throw new TypeError('Invalid object');
+    var keys = [];
+    for (var key in obj) if (_.has(obj, key)) keys[keys.length] = key;
+    return keys;
+  };
 
+
+
+  _.isArray = nativeIsArray || function(obj) {
+    return toString.call(obj) == '[object Array]';
+  };
+
+  // The cornerstone, an `each` implementation, aka `forEach`.
+  // Handles objects with the built-in `forEach`, arrays, and raw objects.
+  // Delegates to **ECMAScript 5**'s native `forEach` if available.
+  var each = _.each = _.forEach = function(obj, iterator, context) {
+    if (obj == null) return;
+    if (nativeForEach && obj.forEach === nativeForEach) {
+      obj.forEach(iterator, context);
+    } else if (obj.length === +obj.length) {
+      for (var i = 0, l = obj.length; i < l; i++) {
+        if (iterator.call(context, obj[i], i, obj) === breaker) return;
+      }
+    } else {
+      for (var key in obj) {
+        if (_.has(obj, key)) {
+          if (iterator.call(context, obj[key], key, obj) === breaker) return;
+        }
+      }
+    }
+  };
+
+
+	
+	
 	return _;	
 });
