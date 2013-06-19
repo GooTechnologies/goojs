84410faa45237aa8e98f8d8570f763a798b90611
Adrian Toncean <adrian.toncean@gooengine.com>
2013-06-19 11:05:33 +0200

    #291: Adapted test for the new sphere

--- test/shapes/Sphere-test.js
+++ test/shapes/Sphere-test.js
@@ -10,8 +10,8 @@ define(
 		var a = ShapeCreator.createSphere(8, 4);
 
 		it("Number of vertices and indices", function() {
-			expect(a.vertexCount).toEqual(32);
-			expect(a.indexCount).toEqual(144);
+			expect(a.vertexCount).toEqual(37);
+			expect(a.indexCount).toEqual(168);
 		});
 	});
 });
