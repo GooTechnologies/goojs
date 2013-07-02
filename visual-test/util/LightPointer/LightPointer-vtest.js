a6eec8e3f8852cf4b6dab11d12334770d4eb5921
Adrian Toncean <adrian.toncean@gooengine.com>
2013-07-02 11:39:37 +0200

    #325 Added LightPointer

---
+++ visual-test/util/LightPointer/LightPointer-vtest.js
@@ -0,0 +1,169 @@
+require.config({
+	paths: {
+		"goo": "../../../src/goo"
+	}
+});
+
+require([
+	'goo/entities/GooRunner',
+	'goo/entities/World',
+	'goo/renderer/Material',
+	'goo/renderer/shaders/ShaderLib',
+	'goo/renderer/Camera',
+	'goo/shapes/ShapeCreator',
+	'goo/entities/components/CameraComponent',
+	'goo/scripts/OrbitCamControlScript',
+	'goo/entities/EntityUtils',
+	'goo/entities/components/ScriptComponent',
+	'goo/renderer/MeshData',
+	'goo/entities/components/MeshRendererComponent',
+	'goo/math/Vector3',
+	'goo/renderer/light/PointLight',
+	'goo/renderer/light/DirectionalLight',
+	'goo/renderer/light/SpotLight',
+	'goo/entities/components/LightComponent',
+	'goo/util/LightPointer'
+], function (
+	GooRunner,
+	World,
+	Material,
+	ShaderLib,
+	Camera,
+	ShapeCreator,
+	CameraComponent,
+	OrbitCamControlScript,
+	EntityUtils,
+	ScriptComponent,
+	MeshData,
+	MeshRendererComponent,
+	Vector3,
+	PointLight,
+	DirectionalLight,
+	SpotLight,
+	LightComponent,
+	LightPointer
+	) {
+	'use strict';
+
+	function addSpin(entity, radius, speed, altitude) {
+		entity.setComponent(new ScriptComponent({
+			run: function (entity) {
+				entity.transformComponent.transform.translation.setd(
+					Math.cos(World.time * speed) * radius,
+					altitude,
+					Math.sin(World.time * speed) * radius
+				);
+				entity.transformComponent.transform.setRotationXYZ(
+					0,
+					-World.time * speed - Math.PI/2,
+					0
+				);
+				entity.transformComponent.setUpdated();
+			}
+		}));
+	}
+
+	function lightPointerDemo(goo) {
+		// add spheres to cast light on
+		var sphereMeshData = ShapeCreator.createSphere();
+
+		var sphereMaterial = Material.createMaterial(ShaderLib.simpleLit, 'SphereMaterial');
+
+		var nSpheres = 15;
+		for(var i = 0; i < nSpheres; i++) {
+			for(var j = 0; j < nSpheres; j++) {
+				var sphereEntity = EntityUtils.createTypicalEntity(goo.world, sphereMeshData);
+				sphereEntity.transformComponent.transform.translation.set(i - nSpheres/2, j - nSpheres/2, 0);
+				sphereEntity.meshRendererComponent.materials.push(sphereMaterial);
+				sphereEntity.addToWorld();
+			}
+		}
+
+		// add point light ----------------------------------------------------
+		var pointLight = new PointLight();
+		pointLight.color.data[0] = 0.9;
+		pointLight.color.data[1] = 0.0;
+		pointLight.color.data[2] = 0.2;
+		pointLight.range = 10;
+
+		var pointLightOrbitRadius = 5;
+		var pointLightOrbitSpeed = 0.5;
+		var pointLightAltitude = 0;
+
+		var pointLightEntity = goo.world.createEntity('pointLight');
+		pointLightEntity.setComponent(new LightComponent(pointLight));
+
+		LightPointer.attachPointer(pointLightEntity);
+
+		addSpin(pointLightEntity, pointLightOrbitRadius, pointLightOrbitSpeed, pointLightAltitude);
+		pointLightEntity.addToWorld();
+		goo.world.process();
+
+
+		// add directional light ----------------------------------------------
+		var directionalLight = new DirectionalLight();
+		directionalLight.color.data[0] = 0.2;
+		directionalLight.color.data[1] = 0.9;
+		directionalLight.color.data[2] = 0.0;
+
+		var directionalLightOrbitRadius = 0;
+		var directionalLightOrbitSpeed = 0.7;
+		var directionalLightAltitude = -5;
+
+		var directionalLightEntity = goo.world.createEntity('directionalLight');
+		directionalLightEntity.setComponent(new LightComponent(directionalLight));
+
+		LightPointer.attachPointer(directionalLightEntity);
+
+		addSpin(directionalLightEntity, directionalLightOrbitRadius, directionalLightOrbitSpeed, directionalLightAltitude);
+		directionalLightEntity.addToWorld();
+		goo.world.process();
+
+
+		// add spot light -----------------------------------------------------
+		var spotLight = new SpotLight();
+		spotLight.color.data[0] = 0.0;
+		spotLight.color.data[1] = 0.2;
+		spotLight.color.data[2] = 0.9;
+		spotLight.angle = 15;
+		spotLight.range = 10;
+
+		var spotLightOrbitRadius = 5;
+		var spotLightOrbitSpeed = 0.3;
+		var spotLightAltitude = 5;
+
+		var spotLightEntity = goo.world.createEntity('spotLight');
+		spotLightEntity.setComponent(new LightComponent(spotLight));
+
+		LightPointer.attachPointer(spotLightEntity);
+
+		addSpin(spotLightEntity, spotLightOrbitRadius, spotLightOrbitSpeed, spotLightAltitude);
+		spotLightEntity.addToWorld();
+		goo.world.process();
+
+
+		// camera
+		var camera = new Camera(45, 1, 1, 1000);
+		var cameraEntity = goo.world.createEntity("CameraEntity");
+		cameraEntity.transformComponent.transform.translation.set(0, 0, 3);
+		cameraEntity.transformComponent.transform.lookAt(new Vector3(0, 0, 0), Vector3.UNIT_Y);
+		cameraEntity.setComponent(new CameraComponent(camera));
+		cameraEntity.addToWorld();
+		var scripts = new ScriptComponent();
+		scripts.scripts.push(new OrbitCamControlScript({
+			domElement : goo.renderer.domElement,
+			spherical : new Vector3(5, Math.PI / 2, 0)
+		}));
+		cameraEntity.setComponent(scripts);
+	}
+
+	function init() {
+		var goo = new GooRunner();
+		goo.renderer.domElement.id = 'goo';
+		document.body.appendChild(goo.renderer.domElement);
+
+		lightPointerDemo(goo);
+	}
+
+	init();
+});
