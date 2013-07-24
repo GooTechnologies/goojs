6102697ebfebaf6c2244ad36fa71f1c4d3c7411f
Adrian Toncean <adrian.toncean@gooengine.com>
2013-07-24 12:04:04 +0200

    #376 Added a way to view camera frustrums

---
+++ src/goo/util/FrustrumViewer.js
@@ -0,0 +1,153 @@
+define([
+	'goo/renderer/MeshData',
+	'goo/util/MeshBuilder',
+	'goo/math/Transform',
+	'goo/shapes/ShapeCreator',
+	'goo/entities/components/MeshDataComponent',
+	'goo/entities/components/MeshRendererComponent',
+	'goo/renderer/Material',
+	'goo/renderer/shaders/ShaderLib'
+	],
+	/* @lends */
+	function (
+		MeshData,
+		MeshBuilder,
+		Transform,
+		ShapeCreator,
+		MeshDataComponent,
+		MeshRendererComponent,
+		Material,
+		ShaderLib
+	) {
+	"use strict";
+
+	function FrustrumViewer() {
+	}
+
+	function buildFrustrum(fov, aspect, near, far) {
+		var angle = (fov * Math.PI/180) / 2;
+		var sine = Math.sin(angle);
+
+		var f0, f1, f2, f3;
+		f0 = {
+			x: -sine * far,
+			y:  sine * far,
+			z: -far
+		};
+
+		f1 = {
+			x: -sine * far,
+			y: -sine * far,
+			z: -far
+		};
+
+		f2 = {
+			x:  sine * far,
+			y: -sine * far,
+			z: -far
+		};
+
+		f3 = {
+			x:  sine * far,
+			y:  sine * far,
+			z: -far
+		};
+
+		var n0, n1, n2, n3;
+		n0 = {
+			x: -sine * near,
+			y:  sine * near,
+			z: -near
+		};
+
+		n1 = {
+			x: -sine * near,
+			y: -sine * near,
+			z: -near
+		};
+
+		n2 = {
+			x:  sine * near,
+			y: -sine * near,
+			z: -near
+		};
+
+		n3 = {
+			x:  sine * near,
+			y:  sine * near,
+			z: -near
+		};
+
+		var verts = [];
+		verts.push(f0.x, f0.y, f0.z);
+		verts.push(f1.x, f1.y, f1.z);
+		verts.push(f2.x, f2.y, f2.z);
+		verts.push(f3.x, f3.y, f3.z);
+
+		verts.push(n0.x, n0.y, n0.z);
+		verts.push(n1.x, n1.y, n1.z);
+		verts.push(n2.x, n2.y, n2.z);
+		verts.push(n3.x, n3.y, n3.z);
+
+		var indices = [];
+		indices.push(0, 1);
+		indices.push(1, 2);
+		indices.push(2, 3);
+		indices.push(3, 0);
+
+		indices.push(4, 5);
+		indices.push(5, 6);
+		indices.push(6, 7);
+		indices.push(7, 4);
+
+		indices.push(0, 4);
+		indices.push(1, 5);
+		indices.push(2, 6);
+		indices.push(3, 7);
+
+		var meshData = new MeshData(MeshData.defaultMap([MeshData.POSITION]), 8, 24);
+
+		meshData.getAttributeBuffer(MeshData.POSITION).set(verts);
+		meshData.getIndexBuffer().set(indices);
+
+		meshData.indexLengths = null;
+		meshData.indexModes = ['Lines'];
+
+		return meshData;
+	}
+
+	/**
+	 * Attaches a pointer mesh to the specifies light entity that represents the light's properties (color, position, direction and range)
+	 * @param {Entity} light entity to attach the pointer mesh to
+	 * @return {Entity} the light entity for chaining.
+	 */
+	FrustrumViewer.attachGuide = function (cameraEntity) {
+		var camera = cameraEntity.getComponent('CameraComponent').camera;
+
+		var meshData = buildFrustrum(camera.fov, camera.aspect, camera.near, camera.far - camera.far/1000);
+		var meshDataComponent = new MeshDataComponent(meshData);
+		cameraEntity.setComponent(meshDataComponent);
+
+		var meshRendererComponent = new MeshRendererComponent();
+		var material = Material.createMaterial(ShaderLib.simpleColored, '');
+		material.uniforms.color = [0.5, 0.7, 1];
+		meshRendererComponent.materials.push(material);
+		cameraEntity.setComponent(meshRendererComponent);
+
+		return cameraEntity;
+	};
+
+	/**
+	 * Removes any mesh attached to an entity with a light component
+	 * @param {Entity} light entity to remove the mesh components from
+	 * @return {Entity} the light entity for chaining.
+	 */
+	FrustrumViewer.removeMesh = function (cameraEntity) {
+		cameraEntity.clearComponent('meshDataComponent');
+		cameraEntity.clearComponent('meshRendererComponent');
+
+		return cameraEntity;
+	};
+
+	return FrustrumViewer;
+});
