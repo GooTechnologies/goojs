c0edfbcee494d9b0396e13c983c525f579005ff9
Adrian Toncean <adrian.toncean@gooengine.com>
2013-08-06 09:23:40 +0200

    Added bounding polygon script

---
+++ src/goo/scripts/PolyBoundingScript.js
@@ -0,0 +1,46 @@
+require.config({
+	paths: {
+		"goo/lib": "goojs/lib"
+	}
+});
+
+define(['goo/lib/polyk'],
+	function(PolyK) {
+	"use strict";
+
+	function PolyBoundingScript() {
+		this.collidables = [];
+	}
+
+	PolyBoundingScript.prototype.addCollidable = function(collidable) {
+		this.collidables.push(collidable);
+	};
+
+	PolyBoundingScript.prototype.removeAllAt = function(x, y, z) {
+		this.collidables = this.collidables.filter(function(collidable) {
+			if(collidable.bottom <= z && collidable.top >= z) {
+				return !PolyK.ContainsPoint(collidable.poly, x, y);
+			}
+		});
+	};
+
+	PolyBoundingScript.prototype.run = function(entity) {
+		var transformComponent = entity.transformComponent;
+		var translation = transformComponent.transform.translation;
+
+		for (var i in this.collidables) {
+			var collidable = this.collidables[i];
+			if(collidable.bottom <= translation.data[1] && collidable.top >= translation.data[1]) {
+				if(PolyK.ContainsPoint(collidable.poly, translation.data[0], translation.data[2])) {
+					var pointOutside = PolyK.ClosestEdge(collidable.poly, translation.data[0], translation.data[2]);
+					translation.data[0] = pointOutside.x;
+					translation.data[2] = pointOutside.y;
+					transformComponent.setUpdated();
+					return ;
+				}
+			}
+		}
+	};
+
+	return PolyBoundingScript;
+});
