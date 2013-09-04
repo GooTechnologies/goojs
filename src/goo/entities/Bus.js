1f0932aacbb2df3fe0d64d47519d5c6d46ba8b0f
Adrian Toncean <adrian.toncean@gooengine.com>
2013-09-04 17:38:12 +0200

    #458 Optimised bus

--- src/goo/entities/Bus.js
+++ src/goo/entities/Bus.js
@@ -4,8 +4,14 @@ define(
 	"use strict";
 
 	function Bus() {
-		// rewrite this as a trie-like structure
-		this.listeners = [];
+		this.trie = { name: '', listeners: [], children: [] }; // *: { listeners: {name, callback}, children: * }
+	}
+
+	function removeFromArray(array, element) {
+		var index = array.indexOf(element);
+		if (index !== -1) {
+			array.splice(index, 1);
+		}
 	}
 
 	/**
@@ -20,15 +26,40 @@ define(
 		}
 
 		for (var i = 0; i < channels.length; i++) {
-			var channel = channels[i];
+			this._emitToSingle(channels[i], data);
+		}
+	};
 
-			for (var j = 0; j < this.listeners.length; j++) {
-				var listener = this.listeners[j];
+	Bus.prototype._emitToSingle = function(channelName, data) {
+		// this whole bit is common to all
+		var node = this.trie;
+		var channelPath = channelName.split('.');
 
-				if(listener.name.substr(0, channel.length) === channel) {
-					listener.callback(data);
+		for (var i = 0; i < channelPath.length; i++) {
+			var channelSub = channelPath[i];
+
+			// advance to next level
+			var advanced = false;
+			for (var j = 0; j < node.children.length; j++) {
+				if (node.children[j].name === channelSub) {
+					node = node.children[j];
+					advanced = true;
+					break;
 				}
 			}
+			if (!advanced) { return; }
+		}
+		///
+
+		this._emitToAll(node, data);
+	};
+
+	Bus.prototype._emitToAll = function(node, data) {
+		for (var i = 0; i < node.listeners.length; i++) {
+			node.listeners[i](data);
+		}
+		for (var i = 0; i < node.children.length; i++) {
+			this._emitToAll(node.children[i], data);
 		}
 	};
 
@@ -37,19 +68,56 @@ define(
 	 * @param {String} channelName
 	 */
 	Bus.prototype.addListener = function (channelName, callback) {
+		var node = this.trie;
+		var channelPath = channelName.split('.');
+
+		for (var i = 0; i < channelPath.length; i++) {
+			var channelSub = channelPath[i];
+
+			var exists = false;
+			for (var j = 0; j < node.children.length; j++) {
+				if (node.children[j].name === channelSub) {
+					node = node.children[j];
+					exists = true;
+					break;
+				}
+			}
+			if (!exists) {
+				var newNode = { name: channelSub, listeners: [], children: [] };
+				node.children.push(newNode);
+				node = newNode;
+			}
+		}
+
+		node.listeners.push(callback);
+
 		// verify if listener is already present on the same channel
-		this.listeners.push({ name: channelName, callback: callback });
 	};
 
 	/**
-	 * Remove a listener from a channel
+	 * Remove a listener from a channel but not from its children
 	 * @param channelName
 	 * @param callbackToRemove
 	 */
 	Bus.prototype.removeListener = function (channelName, callbackToRemove) {
-		this.listeners = this.listeners.filter(function(listener) {
-			return !(listener.name === channelName && listener.callback === callbackToRemove);
-		});
+		var node = this.trie;
+		var channelPath = channelName.split('.');
+
+		for (var i = 0; i < channelPath.length; i++) {
+			var channelSub = channelPath[i];
+
+			var exists = false;
+			for (var j = 0; j < node.children.length; j++) {
+				if (node.children[j].name === channelSub) {
+					node = node.children[j];
+					exists = true;
+					break;
+				}
+			}
+			if (!exists) { return; }
+		}
+
+		removeFromArray(node.listeners, callbackToRemove);
 	};
 
 	/**
@@ -57,19 +125,41 @@ define(
 	 * @param channelName
 	 */
 	Bus.prototype.removeAllOnChannel = function(channelName) {
-		this.listeners = this.listeners.filter(function(listener) {
-			return listener.name !== channelName;
-		});
+		var node = this.trie;
+		var channelPath = channelName.split('.');
+
+		for (var i = 0; i < channelPath.length; i++) {
+			var channelSub = channelPath[i];
+
+			var exists = false;
+			for (var j = 0; j < node.children.length; j++) {
+				if (node.children[j].name === channelSub) {
+					node = node.children[j];
+					exists = true;
+					break;
+				}
+			}
+			if (!exists) { return; }
+		}
+
+		node.listeners = [];
 	};
+
+	//Bus.prototype.removeChannel (and children)
 
 	/**
 	 * Removes a listener from all channels
 	 * @param callbackToRemove
 	 */
 	Bus.prototype.removeListenerFromAllChannels = function(callbackToRemove) {
-		this.listeners = this.listeners.filter(function(listener) {
-			return listener.callback !== callbackToRemove;
-		});
+		this._remove(this.trie, callbackToRemove);
+	};
+
+	Bus.prototype._remove = function(node, callbackToRemove) {
+		removeFromArray(node.listeners, callbackToRemove);
+		for (var i = 0; i < node.children.length; i++) {
+			this._remove(node.children[i], callbackToRemove);
+		}
 	};
 
 	return Bus;
--- test/entities/Bus-test.js
+++ test/entities/Bus-test.js
@@ -10,6 +10,7 @@ define([
 		beforeEach(function() {
 			bus = new Bus();
 		});
+
 		it('can add a listener, emit and capture a message', function() {
 			var gotData;
 			bus.addListener('main', function(data) { gotData = data; });
@@ -56,7 +57,7 @@ define([
 			var gotData1, gotData2, gotData3;
 			bus.addListener('main.first.second', function(data) { gotData1 = data; });
 			bus.addListener('main.first', function(data) { gotData2 = data; });
-			bus.addListener('second', function(data) { gotData3 = data; });
+			bus.addListener('third', function(data) { gotData3 = data; });
 			bus.emit('main.first.second', 123);
 
 			expect(gotData1).toBe(123);
@@ -82,6 +83,29 @@ define([
 			expect(gotData3).toBeUndefined();
 		});
 
+		it('can remove a listener from a channel only', function() {
+			var gotData1, gotData2, gotData3, gotData4;
+			bus.addListener('first', function(data) { gotData1 = data; });
+
+			var secondListener = function(data) { gotData2 = data; };
+			bus.addListener('first.second', secondListener);
+
+			bus.addListener('first.second.third', function(data) { gotData3 = data; });
+			bus.addListener('fourth', function(data) { gotData4 = data; });
+
+			bus.removeListener('first.second', secondListener);
+
+			bus.emit('first', 123);
+			bus.emit('first.second', 234);
+			bus.emit('first.second.third', 345);
+			bus.emit('fourth', 456);
+
+			expect(gotData1).toBe(123);
+			expect(gotData2).toBeUndefined();
+			expect(gotData3).toBe(345);
+			expect(gotData4).toBe(456);
+		});
+
 		it('can remove all listeners from a channel', function() {
 			var gotData1, gotData2, gotData3;
 
@@ -96,6 +120,29 @@ define([
 			expect(gotData1).toBeUndefined();
 			expect(gotData2).toBeUndefined();
 			expect(gotData3).toBe(321);
+		});
+
+		it('can remove all listeners from a channel only', function() {
+			var gotData1, gotData2, gotData3, gotData4, gotData5;
+
+			bus.addListener('first', function(data) { gotData1 = data; });
+			bus.addListener('first.second', function(data) { gotData2 = data; });
+			bus.addListener('first.second', function(data) { gotData3 = data; });
+			bus.addListener('first.second.third', function(data) { gotData4 = data; });
+			bus.addListener('fourth', function(data) { gotData5 = data; });
+
+			bus.removeAllOnChannel('first.second');
+
+			bus.emit('first', 123);
+			bus.emit('first.second', 234);
+			bus.emit('first.second.third', 345);
+			bus.emit('fourth', 456);
+
+			expect(gotData1).toBe(123);
+			expect(gotData2).toBeUndefined();
+			expect(gotData3).toBeUndefined();
+			expect(gotData4).toBe(345);
+			expect(gotData5).toBe(456);
 		});
 
 		it('can remove a listener from all channels', function() {
