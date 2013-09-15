9d036f056c806ab7ba1c0770604e22dda80c6b2e
Adrian Toncean <adrianton3@gmail.com>
2013-09-15 11:41:35 +0200

    #451 Refactored machine handler

--- src/goo/loaders/handlers/MachineHandler.js
+++ src/goo/loaders/handlers/MachineHandler.js
@@ -39,34 +39,50 @@ define([
 	MachineHandler.prototype = Object.create(ConfigHandler.prototype);
 	ConfigHandler._registerClass('machine', MachineHandler);
 
+	
+	MachineHandler.prototype._updateActions = function(realState, stateConfig) {
+		for (var j = 0; j < stateConfig.actions.length; j++) {
+			var action = stateConfig.actions[j];
+			if (MachineHandler.actions[action.type] instanceof Function) {
+				var realAction = new MachineHandler.actions[action.type](action.options);
+				realState.addAction(realAction);
+			}
+		}
+	};
+	
+	
+	MachineHandler.prototype._updateState = function(realMachine, stateConfig) {
+		var realState = new State(stateConfig.uuid);
+		
+		realMachine.addState(realState);
+		this._updateActions(realState, stateConfig);
+		
+		
+		function update(ref) {
+			return that.getConfig(ref).then(function(config) {
+				return that.updateObject(ref, config);
+			});
+		}
+		
+		// machine refs
+		var promises = [];
+		for (var j = 0; j < stateConfig.machineRefs.length; j++) {
+			var machine = stateConfig.machineRefs[j];
+
+			//realState._machines.push(machine.name);			
+		}
+	};
+	
 	MachineHandler.prototype.update = function(ref, config) {
 		var realMachine = new Machine(config.name);
 
 		// states
 		for (var i = 0; i < config.states.length; i++) {
-			var state = config.states[i];
-			var realState = new State(state.uuid);
-			realMachine.addState(realState);
-
-			// actions
-			for (var j = 0; j < state.actions.length; j++) {
-				var action = state.actions[j];
-				if (MachineHandler.actions[action.type] instanceof Function) {
-					var realAction = new MachineHandler.actions[action.type](action.options);
-					state.addAction(realAction);
-				}
-			}
-
-			// machine refs
-			for (var j = 0; j < state.machineRefs.length; j++) {
-				var machine = state.machines[j];
-
-				realState._machines.push(machine.name);
-				// store them initially as uuids and after every state is loaded do a replace on all
-			}
+			this._updateState(realMachine, config.states[i]);
 		}
 
 		// vars
+		// ...
 
 		return PromiseUtil.createDummyPromise(realMachine);
 	};
