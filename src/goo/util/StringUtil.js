bf77fd1f95afefc1743de28a40b94e3b6506af57
Martin Berg <martin.berg@gooengine.com>
2013-09-15 17:40:11 +0200

    Useful additions to StringUtil

--- src/goo/util/StringUtil.js
+++ src/goo/util/StringUtil.js
@@ -13,6 +13,48 @@ define(function() {
 		return str.substring(0,1).toUpperCase()+str.substring(1);
 	};
 
+	StringUtil.getIndexedName = function(base, takenNames, separator){
+		if (!separator) separator = '_'
+
+		var re = RegExp(base+'(' + separator + '\\d+)?');
+		var i;
+		var index = 0;
+		for (i in takenNames) {
+			var name = takenNames[i]
+			var m = re.exec(name);
+			if (m) {
+				if (m.length>1 && m[1]){
+					var nidx = parseInt(m[1].substring(separator.length));
+					if (nidx>=index) index = nidx+1;
+				}
+				else {
+					index = 1;
+				}
+			}
+		}
+		
+		return base + separator + index;
+	}
+
+	StringUtil.getUniqueName = function(desiredName, takenNames, separator) {
+		if (takenNames.indexOf(desiredName)===-1)
+			return desiredName;
+
+		return StringUtil.getIndexedName(desiredName, takenNames, separator)
+	}
+
+
+	StringUtil.hashCode = function(str){
+		var hash = 0;
+		if (str.length == 0) return hash;
+		for (i = 0; i < str.length; i++) {
+			char = str.charCodeAt(i);
+			hash = ((hash<<5)-hash)+char;
+			hash = hash & hash; // Convert to 32bit integer
+		}
+		return btoa(hash);
+	}
+
 
 	return StringUtil;
 });
