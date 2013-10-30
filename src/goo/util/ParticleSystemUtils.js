511f13191a9930dee9d513aec0903874020262a6
Adrian Toncean <adrian.toncean@gooengine.com>
2013-10-30 11:29:53 +0100

    Added radial-sparks and plankton texture generators

--- src/goo/util/ParticleSystemUtils.js
+++ src/goo/util/ParticleSystemUtils.js
@@ -65,7 +65,106 @@ define([
 		}
 
 		con2d.fillStyle = gradient;
-		con2d.fillRect(0, 0, size, size);;
+		con2d.fillRect(0, 0, size, size);
+
+		var imageData = con2d.getImageData(0, 0, size, size).data;
+		imageData = new Uint8Array(imageData);
+
+		var texture = new Texture(imageData, null, size, size);
+		return texture;
+	};
+
+	ParticleSystemUtils.createSplashTexture = function(size, options) {
+		size = size || 64;
+
+		options = options || {};
+		options.nTrails = typeof options.nTrails !== 'undefined' ? options.nTrails : 8;
+		options.trailStartRadius = typeof options.trailStartRadius !== 'undefined' ? options.trailStartRadius : 1;
+		options.trailEndRadius = typeof options.trailEndRadius !== 'undefined' ? options.trailEndRadius : 4;
+
+		var canvas = document.createElement('canvas');
+		document.body.appendChild(canvas);
+		canvas.width = size;
+		canvas.height = size;
+		var con2d = canvas.getContext('2d');
+
+		// ----
+		function circle(x, y, r) {
+			var grad = con2d.createRadialGradient(x, y, 0, x, y, r);
+			grad.addColorStop(0, 'rgba(255, 255, 255, 0.5)');
+			grad.addColorStop(1, 'rgba(255, 255, 255, 0)');
+			con2d.fillStyle = grad;
+			con2d.fillRect(x - r, y - r, 2 * r, 2 * r);
+		}
+
+		var nSteps = 30;
+		function trail(sx, sy, ex, ey, sr, er) {
+			var ax = (ex - sx) / nSteps;
+			var ay = (ey - sy) / nSteps;
+			var ar = (er - sr) / nSteps;
+			for (var i = 0, x = sx, y = sy, r = sr; i < nSteps; i++, x+=ax, y+=ay, r+=ar) {
+				circle(x, y, r);
+			}
+		}
+
+		function splash(x, y, minInnerRadius, maxOuterRadius, startTrailRadius, endTrailRadius, n) {
+			for (var i = 0; i < n; i++) {
+				var angle = Math.random() * Math.PI * 2;
+				var innerRadius = Math.random() * 4 + minInnerRadius;
+				var outerRadius = Math.random() * 4 - maxOuterRadius;
+				trail(
+					x + Math.cos(angle) * innerRadius,
+					y + Math.sin(angle) * innerRadius,
+					x + Math.cos(angle) * outerRadius,
+					y + Math.sin(angle) * outerRadius,
+					startTrailRadius,
+					endTrailRadius
+				);
+			}
+		}
+		// ----
+
+		splash(size/2, size/2, ((size/2)/10)*1, ((size/2)/10)*9, options.trailStartRadius, options.trailEndRadius, options.nTrails);
+
+		var imageData = con2d.getImageData(0, 0, size, size).data;
+		imageData = new Uint8Array(imageData);
+
+		var texture = new Texture(imageData, null, size, size);
+		return texture;
+	};
+
+	ParticleSystemUtils.createPlanktonTexture = function(size, options) {
+		size = size || 64;
+		console.log(size);
+		options = options || {};
+		options.nPoints = typeof options.nPoints !== 'undefined' ? options.nPoints : 10;
+		options.minRadius = typeof options.minRadius !== 'undefined' ? options.minRadius : 2;
+		options.maxRadius = typeof options.maxRadius !== 'undefined' ? options.maxRadius : 5;
+
+		var canvas = document.createElement('canvas');
+		document.body.appendChild(canvas);
+		canvas.width = size;
+		canvas.height = size;
+		var con2d = canvas.getContext('2d');
+
+		function circle(x, y, r) {
+			var grad = con2d.createRadialGradient(x, y, 0, x, y, r);
+			grad.addColorStop(0, 'rgba(255, 255, 255, 1)');
+			grad.addColorStop(0.3, 'rgba(255, 255, 255, 1)');
+			grad.addColorStop(1, 'rgba(255, 255, 255, 0)');
+			con2d.fillStyle = grad;
+			con2d.fillRect(x - r, y - r, 2 * r, 2 * r);
+		}
+
+		function soup(n) {
+			for (var i = 0; i < n; i++) {
+				var x = Math.random() * (size - options.maxRadius * 2) + options.maxRadius;
+				var y = Math.random() * (size - options.maxRadius * 2) + options.maxRadius;
+				circle(x, y, Math.random() * (options.maxRadius - options.minRadius) + options.minRadius);
+			}
+		}
+
+		soup(options.nPoints);
 
 		var imageData = con2d.getImageData(0, 0, size, size).data;
 		imageData = new Uint8Array(imageData);
