0dbba1c607cd5eef43a2532a3ce13d547c92d4a9
Adrian Toncean <adrian.toncean@gooengine.com>
2013-10-30 13:01:04 +0100

    #451 Can now set 'speed' of shake action

--- src/goo/statemachine/actions/ShakeAction.js
+++ src/goo/statemachine/actions/ShakeAction.js
@@ -31,13 +31,20 @@ function(
 			key: 'endLevel',
 			type: 'number',
 			description: 'Shake amount at the end',
-			'default': 0
+			'default': 10
 		}, {
 			name: 'Time',
 			key: 'time',
 			type: 'number',
 			description: 'Shake time amount',
 			'default': 1000
+		}, {
+			name: 'Speed',
+			key: 'speed',
+			type: 'dropdown',
+			description: 'Speed of shaking',
+			'default': 'Fast',
+			options: ['Fast', 'Medium', 'Slow']
 		}],
 		transitions: [{
 			key: 'complete',
@@ -50,6 +57,7 @@ function(
 		this.startLevel = settings.startLevel;
 		this.endLevel = settings.endLevel;
 		this.time = settings.time;
+		this.speed = { 'Fast': 1, 'Medium': 2, 'Slow': 4 }[settings.speed];
 		this.easing = window.TWEEN.Easing.Quadratic.InOut;
 		this.eventToEmit = { channel: settings.transitions.complete };
 	};
@@ -63,20 +71,35 @@ function(
 		var transformComponent = entity.transformComponent;
 		var translation = transformComponent.transform.translation;
 
-		var oldRan = new Vector3();
-		var ran = new Vector3();
+		var oldVal = new Vector3();
+		var target = new Vector3();
+		var vel = new Vector3();
 
+		var that = this;
+		var iter = 0;
 		this.tween.from({ level: +this.startLevel }).to({ level: +this.endLevel }, +this.time).easing(this.easing).onUpdate(function() {
-			ran.setd(
-				(Math.random()-0.5) * this.level * 2,
-				(Math.random()-0.5) * this.level * 2,
-				(Math.random()-0.5) * this.level * 2
+			iter++;
+			if (iter > +that.speed) {
+				iter = 0;
+
+				target.setd(
+					(Math.random()-0.5) * this.level * 2,
+					(Math.random()-0.5) * this.level * 2,
+					(Math.random()-0.5) * this.level * 2
+				);
+			}
+
+			vel.setd(
+				vel.data[0] * 0.98 + (target.data[0] - translation.data[0]) * 0.1,
+				vel.data[1] * 0.98 + (target.data[1] - translation.data[1]) * 0.1,
+				vel.data[2] * 0.98 + (target.data[2] - translation.data[2]) * 0.1
 			);
-			translation.add(ran).sub(oldRan);
-			oldRan.copy(ran);
+
+			translation.add(vel).sub(oldVal);
+			oldVal.copy(vel);
 			transformComponent.setUpdated();
 		}).onComplete(function() {
-			translation.sub(oldRan);
+			translation.sub(oldVal);
 			transformComponent.setUpdated();
 			fsm.send(this.eventToEmit.channel);
 		}.bind(this)).start();
