984b5141a129095618277c00e0555050329bfaac
Adrian Toncean <adrian.toncean@gooengine.com>
2013-11-05 11:48:18 +0100

    #451 More descriptive description

--- src/goo/statemachine/actions/CollidesAction.js
+++ src/goo/statemachine/actions/CollidesAction.js
@@ -20,12 +20,12 @@ function(
 
 	CollidesAction.external = {
 		name: 'Collides',
-		description: 'Checks for collisions with other entities',
+		description: 'Checks for collisions with other entities; Collisions are based on the entities\' bounding volumes',
 		parameters: [{
 			name: 'Tag',
 			key: 'tag',
 			type: 'dropdown',
-			description: 'Checks for collisions with other ojects having this tag',
+			description: 'Checks for collisions with other objects having this tag',
 			'default': 'red',
 			options: ['red', 'blue', 'green', 'yellow']
 		}],
