41922f06eacdc5f85b91d250d69d054126126ec7
Dan Nilsson <dan@datacraft.se>
2013-11-27 16:01:13 +0100

    - Undo removed mistake

---
+++ src/goo/loaders/handlers/LogicComponentHandler.js
@@ -0,0 +1,53 @@
+define(['goo/loaders/handlers/ComponentHandler',
+	'goo/entities/components/LogicComponent',
+	'goo/util/rsvp',
+	'goo/util/PromiseUtil',
+	'goo/util/ObjectUtil',
+	'goo/logic/LogicNodeRandom',
+	'goo/logic/LogicNodeVec3',
+	'goo/logic/LogicNodeMultiply',
+	'goo/logic/LogicNodeWASD',
+	'goo/logic/LogicNodeWASD2',
+	'goo/logic/LogicNodeMouse',
+	'goo/logic/LogicNodeAdd',
+	'goo/logic/LogicNodeSub',
+	'goo/logic/LogicNodeFloat',
+	'goo/logic/LogicNodeApplyMatrix',
+	'goo/logic/LogicNodeConstVec3',
+	'goo/logic/LogicNodeVec3Add',
+	'goo/logic/LogicNodeRotationMatrix',
+	'goo/logic/LogicNodeMultiplyFloat',
+	'goo/logic/LogicNodeMax',
+	'goo/logic/LogicNodeInt'
+
+], function(
+	ComponentHandler,
+	LogicComponent,
+	RSVP,
+	pu,
+	_
+) {
+	"use strict";
+
+	function LogicComponentHandler() {
+		ComponentHandler.apply(this, arguments);
+	}
+
+	LogicComponentHandler.prototype = Object.create(ComponentHandler.prototype);
+	LogicComponentHandler.prototype.constructor = LogicComponentHandler;
+	ComponentHandler._registerClass('logic', LogicComponentHandler);
+
+
+	LogicComponentHandler.prototype._create = function(entity, config) {
+		var component = new LogicComponent(light);
+		component.configure(config);
+		entity.setComponent(component);
+		return component;
+	};
+
+	LogicComponentHandler.prototype.update = function(entity, config) {
+		return pu.createDummyPromise(component);
+	};
+
+	return LogicComponentHandler;
+});
