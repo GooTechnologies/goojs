14bcfd35c6d38c4edb387444c7916df93b836843
Adrian Toncean <adrian.toncean@gooengine.com>
2013-12-13 10:47:01 +0100

    Fixed the 'sparse heightmap' visual test

--- visual-test/scripts/HeightMapBoundingScript/HeightMapBoundingScript-vtest.js
+++ visual-test/scripts/HeightMapBoundingScript/HeightMapBoundingScript-vtest.js
@@ -117,8 +117,8 @@ require([
 			// Add camera
 			var camera = new Camera(45, 1, 1, 1000);
 			var cameraEntity = goo.world.createEntity("CameraEntity");
-			cameraEntity.transformComponent.transform.translation.set(0, 0, 20);
-			cameraEntity.transformComponent.transform.lookAt(new Vector3(0, 0, 0), Vector3.UNIT_Y);
+			cameraEntity.transformComponent.transform.translation.set(0, 10, 0);
+			cameraEntity.transformComponent.transform.lookAt(new Vector3(30, 0, 30), Vector3.UNIT_Y);
 			cameraEntity.setComponent(new CameraComponent(camera));
 			cameraEntity.addToWorld();
 
@@ -132,7 +132,7 @@ require([
 			scripts.scripts.push(new MouseLookControlScript({
 				domElement : goo.renderer.domElement
 			}));
-			scripts.scripts.push(heightMapBoundingScript);
+			//scripts.scripts.push(heightMapBoundingScript);
 			cameraEntity.setComponent(scripts);
 		});
 	}
