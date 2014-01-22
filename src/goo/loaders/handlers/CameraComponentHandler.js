20309ce390326103d25ac0c5d5ae57783d8fbff5
Rickard Berg <rickard@emerbee.se>
2014-01-22 11:44:27 +0100

    Refactored camera component handler #824

--- src/goo/loaders/handlers/CameraComponentHandler.js
+++ src/goo/loaders/handlers/CameraComponentHandler.js
@@ -15,58 +15,79 @@ define([
 ) {
 	"use strict";
 
+	/*
+	 * @class For handling loading of camera components
+	 * @constructor
+	 * @param {World} world The goo world
+	 * @param {function} getConfig The config loader function. See {@see DynamicLoader._loadRef}.
+	 * @param {function} updateObject The handler function. See {@see DynamicLoader.update}.
+	 * @extends ComponentHandler
+	 */
 	function CameraComponentHandler() {
 		ComponentHandler.apply(this, arguments);
+		this._type = 'CameraComponent';
 	}
 
 	CameraComponentHandler.prototype = Object.create(ComponentHandler.prototype);
 	ComponentHandler._registerClass('camera', CameraComponentHandler);
 	CameraComponentHandler.prototype.constructor = CameraComponentHandler;
 
+	/*
+	 * Prepare component. Set defaults on config here.
+	 * @param {object} config
+	 * @private
+	 */
 	CameraComponentHandler.prototype._prepare = function(config) {
-		return _.defaults(config, {
-			fov: 45,
+		_.defaults(config, {
 			near: 1,
 			far: 10000,
 			projectionMode: 0,
-			size: 100,
 			aspect: 1,
 			lockedRatio: false
 		});
+		if (config.projectionMode === 0 && config.fov === undefined) {
+			config.fov = 45;
+		}
+		if (config.projectionMode === 1 && config.size === undefined) {
+			config.size = 100;
+		}
 	};
 
-	CameraComponentHandler.prototype._create = function(entity/*, config*/) {
+	/*
+	 * Create camera component object based on the config.
+	 * @param {Entity} entity The entity on which this component should be added.
+	 * @returns {CameraComponent} the created component object
+	 * @private
+	 */
+	CameraComponentHandler.prototype._create = function(entity) {
 		var camera = new Camera(45, 1, 1, 1000);
 		var component = new CameraComponent(camera);
 		entity.setComponent(component);
 		return component;
 	};
 
-	CameraComponentHandler.prototype.update = function(entity, config) {
-		var component = ComponentHandler.prototype.update.call(this, entity, config);
-		component.camera.setProjectionMode(config.projectionMode);
-		component.camera.lockedRatio = config.lockedRatio || false;
-		if (config.projectionMode === 0) {
-			component.camera.setFrustumPerspective(config.fov, config.aspect || 1, config.near, config.far);
-		} else {
-			var size = config.size;
-			component.camera.setFrustum(config.near, config.far, -size, size, size, -size, config.aspect || 1);
-			component.camera.size = size;
-		}
-		return pu.createDummyPromise(component);
-	};
+	// TODO: Handle if cameracomponent is removed and camera is active
 
-	CameraComponentHandler.prototype.remove = function(entity) {
-		// This removes the camera entity,
-		// but there is still a visible view that isn't updated.
-		// Perhaps change the engine so it draws just black if
-		// there is no camera?
-		/*jshint eqeqeq: false, -W041*/
-
-		/*if (entity != null && entity.cameraComponent != null && entity.cameraComponent.camera != null) {
-			this.world.removeEntity(entity.cameraComponent.camera);
-		}*/
-		return ComponentHandler.prototype.remove.call(this, entity);
+	/**
+	 * Update engine cameracomponent object based on the config.
+	 * @param {Entity} entity The entity on which this component should be added.
+	 * @param {object} config
+	 * @param {object} options
+	 * @returns {RSVP.Promise} promise that resolves with the component when loading is done.
+	 */
+	CameraComponentHandler.prototype.update = function(entity, config, options) {
+		return ComponentHandler.prototype.update.call(this, entity, config, options).then(function(component) {
+			component.camera.setProjectionMode(config.projectionMode);
+			component.camera.lockedRatio = config.lockedRatio || false;
+			if (config.projectionMode === 0) {
+				component.camera.setFrustumPerspective(config.fov, config.aspect || 1, config.near, config.far);
+			} else {
+				var size = config.size;
+				component.camera.setFrustum(config.near, config.far, -size, size, size, -size, config.aspect || 1);
+				component.camera.size = size;
+			}
+			return component;
+		});
 	};
 
 	return CameraComponentHandler;
---
+++ test/loaders/handlers/CameraComponentHandler-test.js
@@ -0,0 +1,53 @@
+define([
+	'goo/entities/World',
+	'goo/entities/components/CameraComponent',
+	'goo/renderer/Camera',
+	'goo/loaders/DynamicLoader',
+	'loaders/Configs'
+], function(
+	World,
+	CameraComponent,
+	Camera,
+	DynamicLoader,
+	Configs
+) {
+	'use strict';
+	function wait(promise, time) {
+		time = time || 1;
+		waitsFor(function() { return promise.isResolved; }, 'promise does not get resolved', time);
+	}
+
+	describe('CameraComponentHandler', function() {
+		var loader;
+		beforeEach(function() {
+			var world = new World();
+			loader = new DynamicLoader({
+				world: world,
+				rootPath: './',
+				ajax: false
+			});
+		});
+
+		it('loads an entity with a cameraComponent', function() {
+			var config = Configs.entity(['camera']);
+			loader.preload(Configs.get());
+			var p = loader.load(config.id).then(function(entity) {
+				expect(entity.cameraComponent).toEqual(jasmine.any(CameraComponent));
+				expect(entity.cameraComponent.camera).toEqual(jasmine.any(Camera));
+			});
+			wait(p);
+		});
+		it('loads the correct camera settings', function() {
+			var config = Configs.entity(['camera']);
+			loader.preload(Configs.get());
+			var p = loader.load(config.id).then(function(entity) {
+				var camera = entity.cameraComponent.camera;
+				var cameraConfig = config.components.camera;
+				for (var key in cameraConfig) {
+					expect(camera[key]).toBe(cameraConfig[key]);
+				}
+			});
+			wait(p);
+		});
+	});
+});
