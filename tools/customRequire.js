bb3d28892a4ef967c771ecdc88d91c8069336af9
Adrian Toncean <adrian.toncean@gooengine.com>
2014-02-07 13:01:28 +0100

    Fixed special cases with require parameters

--- tools/customRequire.js
+++ tools/customRequire.js
@@ -1,32 +1,41 @@
 (function () {
 	'use strict';
-	var _moduleBank = {};
+	if (!window.define) {
+		var _moduleBank = {};
 
-	window.define = function (nam, dependencies, callback) {
-		//! AT: might want to turn these off
-		console.log('define', nam);
-		dependencies.forEach(function (dependency) {
-			if (!_moduleBank[dependency]) {
-				console.warn(dependency, 'requested by', nam, 'not declared');
+		window.define = function (nam, dependencies, callback) {
+			//! AT: might want to turn these off
+			console.log('define', nam);
+			var resolvedModules;
+			if (callback instanceof Function) {
+				dependencies.forEach(function (dependency) {
+					if (!_moduleBank[dependency]) {
+						console.warn(dependency, 'requested by', nam, 'not declared');
+					}
+				});
+				resolvedModules = dependencies.map(function (dependency) { return _moduleBank[dependency]; });
+			} else {
+				resolvedModules = [];
+				callback = dependencies;
 			}
-		});
-		var resolvedModules = dependencies.map(function (dependency) { return _moduleBank[dependency]; });
 
-		//! AT: weird adaptation for "exports" module required by rsvp
-		if (dependencies[0] === "exports") {
-			_moduleBank[nam] = {};
-			callback(_moduleBank[nam]);
-		} else {
-			_moduleBank[nam] = callback.apply(null, resolvedModules);
-		}
-	};
-
-	window.require = function (dependencies) {
-		console.log('require');
-		dependencies.forEach(function (dependency) {
-			if (!_moduleBank[dependency]) {
-				console.warn(dependency, 'not declared');
+			//! AT: weird adaptation for "exports" module required by rsvp
+			if (dependencies[0] === "exports") {
+				_moduleBank[nam] = {};
+				callback(_moduleBank[nam]);
+			} else {
+				_moduleBank[nam] = callback.apply(null, resolvedModules);
 			}
-		});
-	};
+		};
+
+		window.require = function (dependencies, callback) {
+			console.log('require');
+			var resolvedModules = dependencies.forEach(function (dependency) {
+				if (!_moduleBank[dependency]) {
+					console.warn(dependency, 'not declared');
+				}
+			});
+			callback.apply(null, resolvedModules);
+		};
+	}
 })();
