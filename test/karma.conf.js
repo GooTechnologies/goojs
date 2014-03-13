168ea9b08e2ef30ac80b99b1a3b53081b1bbcb26
Stefan Hedman <schteppe@gmail.com>
2014-03-13 13:27:47 +0100

    changed karma settings

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function(config) {
 
 	// test results reporter to use
 	// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-	reporters: ['progress','coverage'],
+	reporters: ['dots','coverage'],
 
 
 	// web server port
