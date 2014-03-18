6e47f99c31eb9cba6860055db217f406336c5613
Adrian Toncean <adrian.toncean@gooengine.com>
2014-03-18 11:01:21 +0100

    Post-reviewed https://trello.com/c/fSHpPovg/919-normal-camerasystem-sets-camera-on-added-entity

--- src/goo/entities/components/CameraComponent.js
+++ src/goo/entities/components/CameraComponent.js
@@ -1,13 +1,15 @@
 define([
 	'goo/entities/components/Component',
 	'goo/math/Vector3',
-	'goo/renderer/Camera'
+	'goo/renderer/Camera',
+	'goo/entities/SystemBus'
 ],
 /** @lends */
 function (
 	Component,
 	Vector3,
-	Camera
+	Camera,
+	SystemBus
 ) {
 	'use strict';
 
@@ -23,6 +25,17 @@ function (
 		this.leftVec = new Vector3(-1, 0, 0);
 		this.upVec = new Vector3(0, 1, 0);
 		this.dirVec = new Vector3(0, 0, -1);
+
+		this.api = {
+			//! AT: the component holds no reference to its entity therefore this method could never stay on the component
+			setMain: function () {
+				SystemBus.emit('goo.setCurrentCamera', {
+					camera: this.cameraComponent.camera,
+					entity: this
+				});
+				return this;
+			}
+		};
 	}
 
 	CameraComponent.type = 'CameraComponent';
