26384226671ed2580473624d7d27b3c716a85170
Stefan Hedman <schteppe@gmail.com>
2014-03-19 15:10:09 +0100

    svg related tests https://trello.com/c/mMSKRT0f/1053-2d-data-model-and-loader

--- visual-test/goo/entities/Components/QuadComponent-vtest.js
+++ visual-test/goo/entities/Components/QuadComponent-vtest.js
@@ -4,22 +4,20 @@ require([
 	'goo/renderer/shaders/ShaderLib',
 	'goo/entities/components/QuadComponent',
 	'goo/math/Vector3',
-	'goo/shapes/Box',
-	'goo/shapes/Sphere',
-	'goo/shapes/Torus',
 	'../../lib/V',
 	'goo/renderer/TextureCreator',
+	'goo/renderer/Texture',
+	'goo/util/CanvasUtils'
 ], function(
 	GooRunner,
 	Material,
 	ShaderLib,
 	QuadComponent,
 	Vector3,
-	Box,
-	Sphere,
-	Torus,
 	V,
-	TextureCreator
+	TextureCreator,
+	Texture,
+	CanvasUtils
 ) {
 	'use strict';
 
@@ -28,18 +26,42 @@ require([
 	var goo = V.initGoo();
 	var world = goo.world;
 
-	var material = new Material(ShaderLib.texturedLit);
-	var texture = new TextureCreator().loadTexture2D(resourcePath + '/goo.png');
-	material.setTexture('DIFFUSE_MAP', texture);
-	var quadComponent = new QuadComponent(material);
+	var data =	"<svg xmlns='http://www.w3.org/2000/svg' width='200' height='100'>" +
+				'<rect x="0" y="0" width="200" height="100" stroke="black" stroke-width="3" fill="blue" fill-opacity="0.5" />'+
+				'<circle cx="100" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />'+
+				"</svg>";
 
-	var quad = world.createEntity()
-		.setComponent(quadComponent)
-		.addToWorld();
+	var renderSize = 300;
+	var cu = new CanvasUtils();
+	cu.renderSvgToCanvas(data, {
+		resizeToFit:true,
+		width:renderSize,
+		height:renderSize
+	}, function(canvas){
+		var texture = new Texture(canvas, {}, canvas.width, canvas.height);
+		//new TextureCreator().loadTexture2D(resourcePath + '/goo.png');
+		var material = new Material(ShaderLib.texturedLit);
+		material.blendState.blending = 'CustomBlending';
+		material.renderQueue = 2000;
+		material.dualTransparency = true;
+		material.setTexture('DIFFUSE_MAP', texture);
 
-	quad.set(material);
+		var entity1 = world.createEntity()
+			.setComponent(new QuadComponent(material))
+			.addToWorld();
+
+		var entity2 = world.createEntity()
+			.setComponent(new QuadComponent(material))
+			.addToWorld();
+
+		var scale = new Vector3(10, 10, 10);
+		entity1.transformComponent.setScale(scale);
+		entity2.transformComponent.setScale(scale);
+		entity1.transformComponent.setTranslation(new Vector3(0, 0, -2));
+		entity2.transformComponent.setTranslation(new Vector3(0, 0, 2));
+	});
 
 	V.addLights();
 
-	V.addOrbitCamera(new Vector3(3, Math.PI / 2, 0.3));
+	V.addOrbitCamera(new Vector3(15, Math.PI / 2, 0.3));
 });
