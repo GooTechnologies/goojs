f6dd9a6a4f171c45f767d855707cd674fb86ff86
Rickard Berg <rickard@emerbee.se>
2014-03-20 14:36:02 +0100

    Small fix in scriptcomponenthandler

--- src/goo/loaders/handlers/ScriptComponentHandler.js
+++ src/goo/loaders/handlers/ScriptComponentHandler.js
@@ -1,13 +1,15 @@
 define([
 	'goo/loaders/handlers/ComponentHandler',
 	'goo/entities/components/ScriptComponent',
-	'goo/util/rsvp'
+	'goo/util/rsvp',
+	'goo/util/ObjectUtil'
 ],
 /** @lends */
 function(
 	ComponentHandler,
 	ScriptComponent,
-	RSVP
+	RSVP,
+	_
 ) {
 	"use strict";
 
@@ -34,9 +36,9 @@ function(
 		return ComponentHandler.prototype.update.call(this, entity, config, options).then(function(component) {
 			if (!component) { return; }
 			var promises = [];
-			for(var key in config.scripts) {
-				promises.push(that._load(config.scripts[key].scriptRef, options));
-			}
+			_.forEach(config.scripts, function(script) {
+				promises.push(that._load(script.scriptRef, options));
+			}, null, 'sortValue');
 			return RSVP.all(promises).then(function(scripts) {
 				component.scripts = scripts;
 				return component;
