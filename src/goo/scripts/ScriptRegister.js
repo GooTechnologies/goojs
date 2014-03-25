9596fa150442464f1974667a400423003530e56e
Rickard Berg <rickard@emerbee.se>
2014-03-25 11:46:53 +0100

    Removed some scripts #1098

--- src/goo/scripts/ScriptRegister.js
+++ src/goo/scripts/ScriptRegister.js
@@ -1,13 +1,10 @@
 define([
 	'goo/scripts/Scripts',
+	'goo/scripts/newwave/OrbitNPanControlScript',
 	'goo/scripts/newwave/FlyControlScript',
-	'goo/scripts/newwave/FPCamControlScript',
-	'goo/scripts/newwave/MouseLookScript',
 	'goo/scripts/newwave/OrbitCamControlScript',
 	'goo/scripts/newwave/PanCamScript',
-	'goo/scripts/newwave/OrbitNPanControlScript',
-	'goo/scripts/newwave/PickAndRotateScript',
-	'goo/scripts/newwave/RotationScript',
+	'goo/scripts/newwave/MouseLookScript',
 	'goo/scripts/newwave/WASDScript'
 ], function(Scripts) {
 	'use strict';
