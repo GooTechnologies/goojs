4ce91c8d8d4c5f9cd572caa352d8fffad04a8349
Stefan Hedman <schteppe@gmail.com>
2014-05-08 15:17:11 +0200

    post review https://trello.com/c/K4iuXXSI/1257-optimize-first-few-frames-of-a-goo-project

--- src/goo/renderer/TaskScheduler.js
+++ src/goo/renderer/TaskScheduler.js
@@ -1,15 +1,19 @@
 define([], function () {
 	'use strict';
 
+	// REVIEW: Use a function instead? Anynomous objects are not very debug friendly
+	// REVIEW: function TaskScheduler(){}
 	var TaskScheduler = {};
 
 	TaskScheduler.maxTimePerFrame = 50;
 
 	// Engine loop must be disabled while running this
 	TaskScheduler.each = function (queue, onComplete) {
+		// REVIEW: Replace onComplete callback for promise? Looks kinda weird to mix promises and callbacks in the loadScene.js template file
 		var i = 0;
 
 		function process() {
+			// REVIEW: I dont think performance.now is supported in all browsers, particularly on mobile. Shim?
 			var startTime = performance.now();
 			while (i < queue.length && performance.now() - startTime < TaskScheduler.maxTimePerFrame) {
 				queue[i]();
@@ -17,6 +21,7 @@ define([], function () {
 			}
 
 			if (i < queue.length) {
+				// REVIEW: 4ms is 'lagom'? Should this number be hard-coded?
 				setTimeout(process, 4);
 			} else {
 				onComplete();
