20345424bff530d8a73eb09b323e750584654fa4
Victor Sand <victor.sand@gmail.com>
2014-05-12 10:37:16 +0200

    SystemBus docs

--- src/goo/entities/SystemBus.js
+++ src/goo/entities/SystemBus.js
@@ -1,7 +1,18 @@
-define(['goo/entities/Bus'],
-	/** @lends */
-	function (Bus) {
+define([
+	'goo/entities/Bus'
+],
+/** @lends */
+function (
+	Bus
+) {
+
 	"use strict";
+
+	/**
+	 * @name SystemBus
+	 * @class
+	 * SystemBus is a global instance of the {@link Bus} class.
+	 */
 
 	return new Bus();
 });
