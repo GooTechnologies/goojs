019bce494358ad58df9f9ceaaad47c9bd1b11572
Adrian Toncean <adrian.toncean@gooengine.com>
2014-05-15 18:12:31 +0200

    Optimised how component-methods are injected on entities

--- src/goo/entities/components/TransformComponent.js
+++ src/goo/entities/components/TransformComponent.js
@@ -49,90 +49,91 @@ function (
 
 		this._dirty = true;
 		this._updated = false;
-
-		this.api = {
-			getTranslation: function () {
-				return TransformComponent.prototype.getTranslation.apply(this, arguments);
-			}.bind(this),
-			setTranslation: function () {
-				TransformComponent.prototype.setTranslation.apply(this, arguments);
-				return this.entity;
-			}.bind(this),
-			getScale: function () {
-				return TransformComponent.prototype.getScale.apply(this, arguments);
-			}.bind(this),
-			setScale: function () {
-				TransformComponent.prototype.setScale.apply(this, arguments);
-				return this.entity;
-			}.bind(this),
-			addTranslation: function () {
-				TransformComponent.prototype.addTranslation.apply(this, arguments);
-				return this.entity;
-			}.bind(this),
-			getRotation: function () {
-				return TransformComponent.prototype.getRotation.apply(this, arguments);
-			}.bind(this),
-			addRotation: function () {
-				TransformComponent.prototype.addRotation.apply(this, arguments);
-				return this.entity;
-			}.bind(this),
-			setRotation: function () {
-				TransformComponent.prototype.setRotation.apply(this, arguments);
-				return this.entity;
-			}.bind(this),
-			lookAt: function () {
-				TransformComponent.prototype.lookAt.apply(this, arguments);
-				return this.entity;
-			}.bind(this),
-
-			// attachChild: Entity | Selection, boolean -> this
-			attachChild: function (entity) {
-				this.attachChild(entity.transformComponent);
-				return this.entity;
-			}.bind(this),
-
-			// detachChild: Entity | Selection, boolean -> this
-			detachChild: function (entity) {
-				this.detachChild(entity.transformComponent);
-				return this.entity;
-			}.bind(this),
-
-			children: function () {
-				return new EntitySelection(this.entity).children();
-			}.bind(this),
-
-			parent: function () {
-				return new EntitySelection(this.entity).parent();
-			}.bind(this),
-
-			traverse: function (callback, level) {
-				level = level !== undefined ? level : 0;
-
-				if (callback(this.entity, level) !== false) {
-					for (var i = 0; i < this.children.length; i++) {
-						var childEntity = this.children[i].entity;
-						childEntity.traverse(callback, level + 1);
-					}
-				}
-
-				return this.entity;
-			}.bind(this),
-
-			traverseUp: function (callback) {
-				var transformComponent = this;
-				while (callback(transformComponent.entity) !== false && transformComponent.parent) {
-					transformComponent = transformComponent.parent;
-				}
-
-				return this.entity;
-			}.bind(this)
-		};
 	}
 
 	TransformComponent.type = 'TransformComponent';
 
 	TransformComponent.prototype = Object.create(Component.prototype);
 	TransformComponent.prototype.constructor = TransformComponent;
+
+	TransformComponent.prototype.api = {
+		setTranslation: function () {
+			TransformComponent.prototype.setTranslation.apply(this.transformComponent, arguments);
+			return this;
+		},
+		setRotation: function () {
+			TransformComponent.prototype.setRotation.apply(this.transformComponent, arguments);
+			return this;
+		},
+		setScale: function () {
+			TransformComponent.prototype.setRotation.apply(this.transformComponent, arguments);
+			return this;
+		},
+		lookAt: function () {
+			TransformComponent.prototype.lookAt.apply(this.transformComponent, arguments);
+			return this;
+		},
+
+		getTranslation: function () {
+			return TransformComponent.prototype.getTranslation.apply(this.transformComponent, arguments);
+		},
+		getRotation: function () {
+			return TransformComponent.prototype.getRotation.apply(this.transformComponent, arguments);
+		},
+		getScale: function () {
+			return TransformComponent.prototype.getScale.apply(this.transformComponent, arguments);
+		},
+
+		addTranslation: function () {
+			TransformComponent.prototype.addTranslation.apply(this, arguments);
+			return this;
+		},
+		addRotation: function () {
+			TransformComponent.prototype.addRotation.apply(this, arguments);
+			return this;
+		},
+		// no, there's no addScale
+
+
+		// attachChild: Entity | Selection, boolean -> this
+		attachChild: function (entity) {
+			this.transformComponent.attachChild(entity.transformComponent);
+			return this;
+		},
+		// detachChild: Entity | Selection, boolean -> this
+		detachChild: function (entity) {
+			this.transformComponent.detachChild(entity.transformComponent);
+			return this;
+		},
+
+		children: function () {
+			return new EntitySelection(this).children();
+		},
+		parent: function () {
+			return new EntitySelection(this).parent();
+		},
+
+		traverse: function (callback, level) {
+			level = level !== undefined ? level : 0;
+
+			if (callback(this, level) !== false) {
+				for (var i = 0; i < this.transformComponent.children.length; i++) {
+					var childEntity = this.transformComponent.children[i].entity;
+					childEntity.traverse(callback, level + 1);
+				}
+			}
+
+			return this;
+		},
+		traverseUp: function (callback) {
+			var transformComponent = this.transformComponent;
+			while (callback(transformComponent.entity) !== false && transformComponent.parent) {
+				transformComponent = transformComponent.parent;
+			}
+
+			return this;
+		}
+	};
 
 	/**
 	 * Gets the value of transformComponent.transform.translation.
