22314780c823fd3f76674f19ab2c3cb572ecf2e3
Stefan Hedman <schteppe@gmail.com>
2014-05-26 14:28:53 +0200

    reverted to make post effects work https://trello.com/c/GAimeX1s/1428-engine-optimization

--- src/goo/renderer/Shader.js
+++ src/goo/renderer/Shader.js
@@ -186,11 +186,10 @@ function (
 
 		// Bind attributes
 		//TODO: good?
-		// if (this.attributes) {
+		if (this.attributes) {
 		// if (this.attributes !== record.attributes || shaderInfo.meshData !== record.meshData) {
-		if (shaderInfo.meshData !== record.meshData) {
 			// record.attributes = this.attributes;
-			record.meshData = shaderInfo.meshData;
+			// record.meshData = shaderInfo.meshData;
 			var attributeMap = shaderInfo.meshData.attributeMap;
 
 			var attributes = this.attributes;
