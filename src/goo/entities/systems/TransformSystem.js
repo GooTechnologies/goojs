decebcf34997842f85cab73fd2a175312c0eb7d4
Stefan Hedman <schteppe@gmail.com>
2014-05-28 18:06:02 +0200

    readded the transformsystem optimization https://trello.com/c/GAimeX1s/1428-engine-optimization

--- src/goo/entities/systems/TransformSystem.js
+++ src/goo/entities/systems/TransformSystem.js
@@ -1,7 +1,9 @@
-define(['goo/entities/systems/System'],
-	/** @lends */
-	function (System) {
-	"use strict";
+define([
+	'goo/entities/systems/System'
+],
+/** @lends */
+function (System) {
+	'use strict';
 
 	/**
 	 * @class Processes all entities with transform components, making sure they are up to date and valid according to the "scenegraph"
@@ -9,11 +11,13 @@ define(['goo/entities/systems/System'],
 	 */
 	function TransformSystem() {
 		System.call(this, 'TransformSystem', ['TransformComponent']);
+		this.numUpdates = 0;
 	}
 
 	TransformSystem.prototype = Object.create(System.prototype);
 
 	TransformSystem.prototype.process = function (entities) {
+		this.numUpdates = 0;
 		var i, transformComponent;
 		for (i = 0; i < entities.length; i++) {
 			transformComponent = entities[i].transformComponent;
@@ -22,16 +26,41 @@ define(['goo/entities/systems/System'],
 				transformComponent.updateTransform();
 			}
 		}
+
+
+		for (i = 0; i < entities.length; i++) {
+			transformComponent = entities[i].transformComponent;
+			if (transformComponent.parent === null && !transformComponent._dirty) {
+				// Root node that is not dirty. We don't need to update it.
+				transformComponent._updated2 = true;
+			} else {
+				transformComponent._updated2 = false;
+			}
+		}
+
+		for (i = 0; i < entities.length; i++) {
+			transformComponent = entities[i].transformComponent;
+			this.updateWorldTransform(transformComponent);
+		}
+
+		// Should not be needed, but safe it up
 		for (i = 0; i < entities.length; i++) {
 			transformComponent = entities[i].transformComponent;
 			if (transformComponent._dirty) {
-				this.updateWorldTransform(transformComponent);
+				transformComponent.updateWorldTransform();
 			}
 		}
 	};
 
 	TransformSystem.prototype.updateWorldTransform = function (transformComponent) {
-		transformComponent.updateWorldTransform();
+		var isRoot = transformComponent.parent === null;
+		var isUpdated = transformComponent._updated2;
+		var hasUpdatedParent = (transformComponent.parent && transformComponent.parent._updated2);
+		if (!isUpdated && (isRoot || hasUpdatedParent)) {
+			this.numUpdates++;
+			transformComponent.updateWorldTransform();
+			transformComponent._updated2 = true;
+		}
 
 		for (var i = 0; i < transformComponent.children.length; i++) {
 			this.updateWorldTransform(transformComponent.children[i]);
