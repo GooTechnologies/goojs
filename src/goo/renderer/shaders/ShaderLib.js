ba9106dd15b258427b1e62028ea2a0c9a912bf81
Rikard Herlitz <rikard@gooengine.com>
2014-06-16 11:44:32 +0200

    Added wrap around handling in shader

--- src/goo/renderer/shaders/ShaderLib.js
+++ src/goo/renderer/shaders/ShaderLib.js
@@ -65,7 +65,8 @@ define([
 			fogColor: [1, 1, 1],
 			shadowDarkness: 0.5,
 			vertexColorAmount: 1.0,
-			lodBias: 0.0
+			lodBias: 0.0,
+			wrapSettings: [0.5, 0.0]
 		},
 		builder: function (shader, shaderInfo) {
 			ShaderBuilder.light.builder(shader, shaderInfo);
