e810d18183ef7c2ebf9a25320c666aee16919f52
Carl Horned <chorned@gooengine.com>
2014-06-19 13:52:28 +0200

    lol, i broke it, fixed now!

--- visual-test/carousel/vtUrls.js
+++ visual-test/carousel/vtUrls.js
@@ -10,9 +10,9 @@
 			"http://127.0.0.1:8003/goojs/visual-test/goo/addons/Sound/Sound-vtest.html",
 			"http://127.0.0.1:8003/goojs/visual-test/goo/misc/FlatwaterAndParticles/FlatwaterAndParticles-vtest.html",
 			"http://127.0.0.1:8003/goojs/visual-test/goo/misc/CombinedIndexModes/CombinedIndexModes-vtest.html",
-			"http://127.0.0.1:8003/goojs/visual-test/goo/components/PortalComponent/PortalComponent-vtest.html"
+			"http://127.0.0.1:8003/goojs/visual-test/goo/components/PortalComponent/PortalComponent-vtest.html",
 			"http://127.0.0.1:8003/goojs/visual-test/goo/passpack/BloomPass/BloomPass.html",
-			"http://127.0.0.1:8003/goojs/visual-test/goo/renderer/texture/AnisotropicFiltering/Anisotropic-vtest.html",
+			"http://127.0.0.1:8003/goojs/visual-test/goo/renderer/texture/AnisotropicFiltering/Anisotropic-vtest.html"
 		]
 	};
 })();
