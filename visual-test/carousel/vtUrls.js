d64230caa5ced096a397f91c0b58b3d7ec75427e
Carl Horned <chorned@gooengine.com>
2014-06-19 13:49:29 +0200

    Carls first commit evergit status!

--- visual-test/carousel/vtUrls.js
+++ visual-test/carousel/vtUrls.js
@@ -11,6 +11,8 @@
 			"http://127.0.0.1:8003/goojs/visual-test/goo/misc/FlatwaterAndParticles/FlatwaterAndParticles-vtest.html",
 			"http://127.0.0.1:8003/goojs/visual-test/goo/misc/CombinedIndexModes/CombinedIndexModes-vtest.html",
 			"http://127.0.0.1:8003/goojs/visual-test/goo/components/PortalComponent/PortalComponent-vtest.html"
+			"http://127.0.0.1:8003/goojs/visual-test/goo/passpack/BloomPass/BloomPass.html",
+			"http://127.0.0.1:8003/goojs/visual-test/goo/renderer/texture/AnisotropicFiltering/Anisotropic-vtest.html",
 		]
 	};
 })();
