a7b5b30d3a3f37b4ff6538f4c7472f858b1b6737
Martin Berg <martin@emerbee.se>
2014-06-25 10:53:46 +0200

    Fixed https://trello.com/c/enyyOUYj/1579-major-duplicating-child-entities-results-in-unusable-entities by reverting from https://trello.com/c/jvHFvHco/1510-normal-dynamicloader-load-blaha-entity-should-add-blaha-entity-to-world-not-just-its-children

--- src/goo/loaders/handlers/TransformComponentHandler.js
+++ src/goo/loaders/handlers/TransformComponentHandler.js
@@ -96,9 +96,10 @@ function(
 			return that.loadObject(ref, options).then(function(entity) {
 				if (entity && entity.transformComponent) {
 					component.attachChild(entity.transformComponent);
-					if (that.world.entityManager.containsEntity(entity) || that.world._addedEntities.indexOf(entity) > -1) {
-						entity.addToWorld();
-					}
+					entity.addToWorld();
+					// if (that.world.entityManager.containsEntity(entity) || that.world._addedEntities.indexOf(entity) > -1) {
+					// 	entity.addToWorld();
+					// }
 				} else {
 					console.error('Failed to add child to transform component');
 				}
