73fe94a3e14298dc5e181ca638f3b5efa7858835
Adrian Toncean <adrian.toncean@gooengine.com>
2014-07-02 16:56:07 +0200

    Fixing the quadComponent culling state https://trello.com/c/hGxGO4lB/1276-normal-quads-and-2d-teams-recieve-light-differently-on-the-front-back

--- visual-test/goo/quadpack/DoubleQuad/DoubleQuad-vtest.js
+++ visual-test/goo/quadpack/DoubleQuad/DoubleQuad-vtest.js
@@ -4,21 +4,32 @@ require([
 	'goo/math/Vector3',
 	'goo/renderer/TextureCreator',
 	'goo/quadpack/DoubleQuad',
-	'lib/V'
+	'lib/V',
+	'goo/renderer/light/PointLight',
+	'goo/debugpack/components/LightDebugComponent',
+	'goo/debugpack/systems/LightDebugSystem'
 ], function (
 	Material,
 	ShaderLib,
 	Vector3,
 	TextureCreator,
 	DoubleQuad,
-	V
+	V,
+	PointLight,
+	LightDebugComponent,
+	LightDebugSystem
 	) {
 	'use strict';
 
 	var goo = V.initGoo();
 	var world = goo.world;
 
-	var material = new Material(ShaderLib.texturedLit);
+	world.setSystem(new LightDebugSystem());
+
+	var material = new Material(ShaderLib.uber);
+	material.blendState.blending = 'CustomBlending';	// Needed if the quad has transparency
+	material.renderQueue = 2000;
+	material.uniforms.discardThreshold = 0.1;
 	var texture = new TextureCreator().loadTexture2D('../../../resources/check.png');
 	material.setTexture('DIFFUSE_MAP', texture);
 
@@ -26,7 +37,8 @@ require([
 	var normalConeEntity = world.createEntity(normalConeMeshData, material).addToWorld();
 	V.showNormals(normalConeEntity);
 
-	V.addLights();
+	world.createEntity(new LightDebugComponent(), [0, 0, -10], new PointLight()).addToWorld();
+	//V.addLights();
 
 	V.addOrbitCamera(new Vector3(25, Math.PI / 2, 0));
 
