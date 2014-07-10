80002adefbc94ee3dec23cefc8be5da8b231275d
Stefan Hedman <schteppe@gmail.com>
2014-07-10 14:42:20 +0200

    added classes to AmmoRegister

--- src/goo/addons/ammopack/AmmoRegister.js
+++ src/goo/addons/ammopack/AmmoRegister.js
@@ -1,20 +1,36 @@
 define([
 	'goo/scripts/Scripts',
-	'goo/addons/ammopack/AmmoSystem',
 	'goo/addons/ammopack/AmmoComponent',
-	'goo/addons/ammopack/AmmoWorkerSystem',
+	'goo/addons/ammopack/AmmoSystem',
 	'goo/addons/ammopack/AmmoWorkerRigidbodyComponent',
-	'goo/addons/ammopack/calculateTriangleMeshShape'
+	'goo/addons/ammopack/AmmoWorkerSystem',
+	'goo/addons/ammopack/BoxCollider',
+	'goo/addons/ammopack/calculateTriangleMeshShape',
+	'goo/addons/ammopack/CapsuleCollider',
+	'goo/addons/ammopack/Collider',
+	'goo/addons/ammopack/ColliderComponent',
+	'goo/addons/ammopack/MeshCollider',
+	'goo/addons/ammopack/PlaneCollider',
+	'goo/addons/ammopack/SphereCollider',
+	'goo/addons/ammopack/TerrainCollider'
 ], function (Scripts) {
 	'use strict';
 
 	var defines = [
 		'goo/scripts/Scripts',
-		'goo/addons/ammopack/AmmoSystem',
 		'goo/addons/ammopack/AmmoComponent',
-		'goo/addons/ammopack/AmmoWorkerSystem',
+		'goo/addons/ammopack/AmmoSystem',
 		'goo/addons/ammopack/AmmoWorkerRigidbodyComponent',
-		'goo/addons/ammopack/calculateTriangleMeshShape'
+		'goo/addons/ammopack/AmmoWorkerSystem',
+		'goo/addons/ammopack/BoxCollider',
+		'goo/addons/ammopack/calculateTriangleMeshShape',
+		'goo/addons/ammopack/CapsuleCollider',
+		'goo/addons/ammopack/Collider',
+		'goo/addons/ammopack/ColliderComponent',
+		'goo/addons/ammopack/MeshCollider',
+		'goo/addons/ammopack/PlaneCollider',
+		'goo/addons/ammopack/SphereCollider',
+		'goo/addons/ammopack/TerrainCollider'
 	];
 
 	for (var i = 1; i < defines.length; i++) {
