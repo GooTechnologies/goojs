86f38653ef66a91d971acf6ca6790b773f8ecc9b
Simon Howe <simon.howe@gooengine.com>
2014-08-08 10:02:35 +0200

    Fix unresolved promise when loading corrupt SVG

--- test/unit/util/CanvasUtils-test.js
+++ test/unit/util/CanvasUtils-test.js
@@ -7,10 +7,8 @@ function (
 	'use strict';
 
 	describe('CanvasUtils', function () {
-		it('renders an SVG to canvas', function (done) {
-			var data =	"<svg xmlns='http://www.w3.org/2000/svg' width='200' height='100'>" +
-						'<rect x="0" y="0" width="200" height="100" fill="blue" />'+
-						"</svg>";
+
+		describe("Rendering an SVG to canvas", function () {
 
 			var renderSize = 300;
 			var options = {
@@ -18,11 +16,30 @@ function (
 				height: renderSize
 			};
 
-			CanvasUtils.renderSvgToCanvas(data, options, function (canvas) {
-				expect(canvas).toEqual(jasmine.any(HTMLCanvasElement));
-				expect(canvas.width).toEqual(options.width);
-				expect(canvas.height).toEqual(options.height);
-				done();
+			it('should create an canvas element with the given dimensions', function (done) {
+
+				var data = "<svg xmlns='http://www.w3.org/2000/svg' width='200' height='100'>" +
+					'<rect x="0" y="0" width="200" height="100" fill="blue" />' +
+					"</svg>";
+
+				CanvasUtils.renderSvgToCanvas(data, options, function (canvas) {
+					expect(canvas).toEqual(jasmine.any(HTMLCanvasElement));
+					expect(canvas.width).toEqual(options.width);
+					expect(canvas.height).toEqual(options.height);
+					done();
+				});
+			});
+
+			describe("when SVG data is corrupt", function () {
+				it('should fire the callback with no argument (undefined)', function (done) {
+
+					var data = "<svg xmlns='http://www.w3.org/2000/svg' oh wait what?";
+
+					CanvasUtils.renderSvgToCanvas(data, options, function (canvas) {
+						expect(canvas).toBeUndefined();
+						done();
+					});
+				});
 			});
 		});
 	});
