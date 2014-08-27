b6451f8f5da2b5add17e28a551a09971e29b164e
Stefan Hedman <schteppe@gmail.com>
2014-08-27 14:27:05 +0200

    updated terrain test

--- visual-test/goo/addons/Cannon/Cannon-terrain-vtest.js
+++ visual-test/goo/addons/Cannon/Cannon-terrain-vtest.js
@@ -51,11 +51,12 @@ require([
 ) {
 	'use strict';
 
+	/* global CANNON */
+
 	var start = Date.now();
 	var nLin = 100,
 		nCol = 30,
-		elementSize = 0.5,
-		boxEntity;
+		elementSize = 0.5;
 
 	var goo = V.initGoo();
 
@@ -67,18 +68,31 @@ require([
 
 	init();
 
-	function addBox(x, y, z) {
+	function createSphere(x, y, z) {
 		var material = V.getColoredMaterial();
-		var radius = 1;
+		var radius = 1.5;
 		var meshData = new Sphere(8, 8, radius);
-		boxEntity = goo.world.createEntity(meshData, material, [x, y, z]).addToWorld();
+		var boxEntity = goo.world.createEntity(meshData, material, [x, y, z]).addToWorld();
 		boxEntity.setComponent(new CannonRigidbodyComponent({
 			mass: 1
 		}));
 		boxEntity.setComponent(new CannonSphereColliderComponent({ radius: radius }));
+		return boxEntity;
 	}
 
-	function getMatrix(flip) {
+	function createBox(x, y, z) {
+		var material = V.getColoredMaterial();
+		var halfExtents = new Vector3(5, 0.5, 2);
+		var meshData = new Box(halfExtents.x * 2, halfExtents.y * 2, halfExtents.z * 2);
+		var boxEntity = goo.world.createEntity(meshData, material, [x, y, z]).addToWorld();
+		boxEntity.setComponent(new CannonRigidbodyComponent({
+			mass: 1
+		}));
+		boxEntity.setComponent(new CannonBoxColliderComponent({ halfExtents: halfExtents }));
+		return boxEntity;
+	}
+
+	function getMatrix() {
 		var matrix = [];
 		var m = Math.cos((Date.now() - start) / 1000 * 0.05);
 		var a = m * 0.2;
@@ -87,7 +101,7 @@ require([
 		for (var i = 0; i < nLin; i++) {
 			matrix.push([]);
 			for (var j = 0; j < nCol; j++) {
-				var value = Math.sin(i * a) * Math.cos(j * b) + 1;
+				var value = Math.sin(i * a) * Math.cos(j * b) / 3 + 1;
 				//value = i * 0.1;
 				//value = (nLin - i -1) * 0.1;
 				//value = (nCol - j - 1) * 0.1;
@@ -136,16 +150,88 @@ require([
 	// 	entity.meshDataComponent.meshData = meshData;
 	// }
 
+	function addCar(chassisEntity, wheelEntities) {
+
+		// Need to do a process to make the CannonSystem add rigid bodies to the entities
+		goo.world.processEntityChanges();
+
+		var world = cannonSystem.world;
+
+        var wheelMaterial = new CANNON.Material("wheelMaterial");
+        var wheelGroundContactMaterial = new CANNON.ContactMaterial(wheelMaterial, world.defaultMaterial, {
+            friction: 0.3,
+            restitution: 0,
+            contactEquationStiffness: 10000
+        });
+
+        // We must add the contact materials to the world
+        world.addContactMaterial(wheelGroundContactMaterial);
+
+        var centerOfMassAdjust = new CANNON.Vec3(0, 0, 0);
+        var chassisBody = chassisEntity.cannonRigidbodyComponent.body;
+
+        // Create the vehicle
+        var vehicle = new CANNON.RigidVehicle({
+            chassisBody: chassisBody,
+            coordinateSystem: new CANNON.Vec3(0, 2, 1) // forward, left, up
+        });
+
+        var axisWidth = 7;
+        var down = new CANNON.Vec3(0, -1, 0);
+
+        /*
+        var wheelBody = wheelEntities[0].cannonRigidbodyComponent.body;
+        vehicle.addWheel({
+            body: wheelBody,
+            position: new CANNON.Vec3(5, 0, axisWidth / 2).vadd(centerOfMassAdjust),
+            axis: new CANNON.Vec3(0, 0, 1),
+            direction: down
+        });
+
+        var wheelBody = wheelEntities[1].cannonRigidbodyComponent.body;
+        vehicle.addWheel({
+            body: wheelBody,
+            position: new CANNON.Vec3(5, 0, -axisWidth / 2).vadd(centerOfMassAdjust),
+            axis: new CANNON.Vec3(0, 0, -1),
+            direction: down
+        });
+
+        var wheelBody = wheelEntities[2].cannonRigidbodyComponent.body;
+        vehicle.addWheel({
+            body: wheelBody,
+            position: new CANNON.Vec3(-5, 0, axisWidth / 2).vadd(centerOfMassAdjust),
+            axis: new CANNON.Vec3(0, 0, 1),
+            direction: down
+        });
+
+        var wheelBody = wheelEntities[3].cannonRigidbodyComponent.body;
+        vehicle.addWheel({
+            body: wheelBody,
+            position: new CANNON.Vec3(-5, 0, -axisWidth / 2).vadd(centerOfMassAdjust),
+            axis: new CANNON.Vec3(0, 0, -1),
+            direction: down
+        });
+
+        // Some damping to not spin wheels too fast
+        for (var i = 0; i < vehicle.wheelBodies.length; i++) {
+            vehicle.wheelBodies[i].angularDamping = 0.4;
+        }
+
+        vehicle.addToWorld(world);
+        vehicle.setMotorSpeed(0.1, 0);
+        */
+	}
+
 	function init() {
 		addTerrain();
-		addBox(0, 4, 0);
+		var boxEntity = createBox(0, 3, 0);
+		addCar(boxEntity, [
+			createSphere(3, 5, -3),
+			createSphere(-3, 5, -3),
+			createSphere(-3, 5, 3),
+			createSphere(3, 5, 3)
+		]);
 		V.addLights();
 		V.addOrbitCamera([50, 0, 0]);
-		/*
-		goo.callbacks.push(function () {
-			updateTerrain();
-			boxEntity.ammoWorkerRigidbodyComponent.activate();
-		});
-		*/
 	}
 });
