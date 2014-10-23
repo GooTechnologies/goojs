22574db46229ad94a30359c348062968a53d7cd4
Adrian Toncean <adrian.toncean@gooengine.com>
2014-10-23 11:26:41 +0200

    Fixed lights not having their transform updated when attached to existing entities

--- test/unit/entities/systems/LightingSystem-test.js
+++ test/unit/entities/systems/LightingSystem-test.js
@@ -1,1 +1,26 @@
-define(['goo/entities/systems/LightingSystem'], function(LightingSystem) {});
+define([
+	'goo/entities/Entity',
+	'goo/entities/components/TransformComponent',
+	'goo/entities/components/LightComponent',
+	'goo/entities/systems/LightingSystem'
+], function (
+	Entity,
+	TransformComponent,
+	LightComponent,
+	LightingSystem
+) {
+	describe('LightingSystem', function () {
+		describe('inserted', function () {
+			it('will update a light\'s transform', function () {
+				var light = jasmine.createSpyObj('Light', ['update']);
+				var lightComponent = new LightComponent(light);
+				var entity = new Entity().setComponent(lightComponent).setComponent(new TransformComponent());
+				var lightingSystem = new LightingSystem();
+
+				lightingSystem.inserted(entity);
+
+				expect(light.update).toHaveBeenCalledWith(entity.transformComponent.worldTransform);
+			});
+		});
+	});
+});
