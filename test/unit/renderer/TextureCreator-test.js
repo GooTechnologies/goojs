67fb660ece4a569cee7d944467a1c13020529268
Adrian Toncean <adrian.toncean@gooengine.com>
2014-11-05 16:02:17 +0100

    Fixed resource paths for karma

--- test/unit/renderer/TextureCreator-test.js
+++ test/unit/renderer/TextureCreator-test.js
@@ -19,14 +19,16 @@ define([
 					done();
 				};
 
+				var prefix = window.__karma__ ? 'base/test/unit/loaders/res/' : 'loaders/res/';
+
 				var images = [
-					'../../visual-test/resources/check.png',
-					'../../visual-test/resources/check-alt.png',
-					'../../visual-test/resources/check.png',
-					'../../visual-test/resources/check-alt.png',
-					'../../visual-test/resources/check.png',
-					'../../visual-test/resources/check-alt.png'
-				];
+					'check.png',
+					'check-alt.png',
+					'check.png',
+					'check-alt.png',
+					'check.png',
+					'check-alt.png'
+				].map(function (path) { return prefix + path; });
 
 				var cubeTexture = textureCreator.loadTextureCube(images, null, onLoaded);
 				//! AT: if this method fail we will never know, there's no reject callback
