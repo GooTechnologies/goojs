36477dae71210a96c00b50f7bdd5f1b5326616ba
Makke <makke01@hotmail.com>
2015-01-26 10:37:37 +0100

    update raycast closest vtest ordering of spheres

--- visual-test/goo/addons/raycastpack/HitSorting/RayCastClosest-vtest.js
+++ visual-test/goo/addons/raycastpack/HitSorting/RayCastClosest-vtest.js
@@ -25,7 +25,7 @@ require([
 	) {
 	'use strict';
 
-	V.describe('10 primitives and a long line, drawing the closest hit triangle and the normal');
+	V.describe('16 primitives and a long line, drawing the closest hit triangle and the normal');
 
 	var goo = V.initGoo();
 	var world = goo.world;
@@ -35,11 +35,14 @@ require([
 	world.setSystem(raySystem);
 	world.setSystem(LRS);
 
-	V.addOrbitCamera(new Vector3(8, Math.PI / 2, 0));
+	var camera = V.addOrbitCamera(new Vector3(16, Math.PI / 1.5, 0.3));
 	//V.addLights();
 
 	var tmpVec1 = new Vector3();
 	var tmpVec2 = new Vector3();
+	var tmpVec3 = new Vector3();
+	var tmpVec4 = new Vector3();
+
 	var drawTriangle = function(triangle, regularMatrix) {
 		var lineStart = tmpVec1.setArray(triangle[0]);
 		var lineEnd = tmpVec2.setArray(triangle[1]);
@@ -57,34 +60,63 @@ require([
 	};
 
 	var drawNormal = function(position, normal) {
+		var lineEnd = tmpVec2.setVector(normal).addVector(position);
+		LRS.drawLine(position, lineEnd, LRS.AQUA);
+	};
 
+	var drawLineArrow = function (lineStart, lineEnd, frac, color) {
+		var lineDir = tmpVec1.setVector(lineEnd).subVector(lineStart);
+		var lineLen = lineDir.length();
+		lineDir.normalize();
+
+		var arrowStartPosition = tmpVec2.setVector(lineDir).mul(lineLen*frac).addVector(lineStart);
+
+		var arrowEndPosition = tmpVec3.setVector(lineDir).mul(-0.1).addVector(arrowStartPosition);
+
+
+		var arrowUpDir = tmpVec4.setVector(Vector3.UNIT_Y).mul(0.1);
+
+		arrowUpDir.addVector(arrowEndPosition);
+
+		LRS.drawLine(arrowStartPosition, arrowUpDir, color);
+
+		var arrowDownDir = arrowUpDir.subVector(arrowEndPosition).mul(-1).addVector(arrowEndPosition);
+
+		LRS.drawLine(arrowStartPosition, arrowDownDir, color);
+	};
+
+	var drawArrowedLine = function (start, end, time, color) {
+		var fracAdd = (time * 0.1) % 0.1;
+		for (var i = 0; i < 10; i++) {
+			var frac = i * 0.1 + fracAdd;
+			drawLineArrow(start, end, frac, color);
+		}
+		LRS.drawLine(start, end, color);
 	};
 
 	var material1 = new Material('Material1', ShaderLib.uber);
 	material1.uniforms.materialAmbient = [0.0, 0.0, 1.0, 1.0];
 
-	for(var i=0;i<10;i++)
+	for(var i=-8;i<8;i++)
 	{
 		var sphere0 = new Sphere(20, 20);
-		var ent = world.createEntity(sphere0, material1, [-1.2, 0, 0]).addToWorld();
+		var ent = world.createEntity(sphere0, material1, [1.2*i, (0.5-Math.random()*0.5)*0.5, (0.5-Math.random()*0.5)*0.5]).addToWorld();
 		raySystem.addEntity(ent, 4);
 	}
 
 	var start = new Vector3(-10,-5,-1);
 	var end = new Vector3(10,5.2,1);
-	
-	var update = function(){
 
-		var height = Math.sin(world.time);
-		start.setDirect(-15, height, Math.sin(-world.time));
-		end.setDirect(15, height+0.3, Math.sin(world.time));
-	
+	var update = function() {
+
+		var height = Math.sin(world.time)*0.1;
+		start.setDirect(-15, height, Math.sin(-world.time) * 0.1);
+		end.setDirect(15, height, Math.sin(world.time) * 0.1);
+
 		var hitResult = raySystem.castClosest(start, end, false);
 
-		if(hitResult.hit)
-		{
-			LRS.drawLine(start, end, LRS.GREEN);
-
+		var color = LRS.GREEN;
+		if (hitResult.hit) {
 			drawTriangle(hitResult.surfaceObject.triangle, hitResult.surfaceObject.rayObject.regularMatrix);
 
 			var hitLocation = tmpVec1;
@@ -94,8 +126,12 @@ require([
 
 			drawNormal(hitLocation, hitNormal);
 		}
-		else LRS.drawLine(start, end, LRS.RED);
+		else color = LRS.RED;
+
+
+		drawArrowedLine(start, end, world.time, color);
 	};
+
 	
 	goo.callbacks.push(update);
 
