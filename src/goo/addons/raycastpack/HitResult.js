8f82a63dc324b65df7c90b3e32f5dfa42093299c
Makke <makke01@hotmail.com>
2015-02-04 11:02:39 +0100

    fix reference problems with vertexWeights

--- src/goo/addons/raycastpack/HitResult.js
+++ src/goo/addons/raycastpack/HitResult.js
@@ -9,6 +9,8 @@ function (Vector3) {
     function HitResult() {
         this.hit = false;
         this.localHitLocation = new Vector3();
+        this._worldHitLocation = new Vector3();
+        this._worldHitLocationSet = false;
         this.vertexWeights = [];
         this.surfaceObject = null;
     }
@@ -16,17 +18,27 @@ function (Vector3) {
     HitResult.prototype.copyFrom = function (hitResult) {
         this.hit = hitResult.hit;
         this.localHitLocation.setVector(hitResult.localHitLocation);
-        this.vertexWeights = hitResult.vertexWeights;
+        this._worldHitLocation.setVector(hitResult._worldHitLocation);
+        this._worldHitLocationSet = hitResult._worldHitLocationSet;
+        this.vertexWeights[0] = hitResult.vertexWeights[0];
+        this.vertexWeights[1] = hitResult.vertexWeights[1];
+        this.vertexWeights[2] = hitResult.vertexWeights[2];
         this.surfaceObject = hitResult.surfaceObject;
     };
 
     //calculates a world space hit location and puts it in the locationStore
-    HitResult.prototype.getWorldHitLocation = function (locationStore) {
-        locationStore.setVector(this.localHitLocation);
+    HitResult.prototype.getWorldHitLocation = function () {
+        ////only calculate worldHitLocation if its not set before
+        //if (!this._worldHitLocationSet)
+        //{
+            this._worldHitLocation.setVector(this.localHitLocation);
 
-        //transform to world coordinates
-        this.surfaceObject.rayObject.regularMatrix.applyPostPoint(locationStore);
-        return locationStore;
+            //transform to world coordinates
+            this.surfaceObject.rayObject.regularMatrix.applyPostPoint(this._worldHitLocation);
+            this._worldHitLocationSet = true;
+        //}
+
+        return this._worldHitLocation;
     };
 
     //not necessary I think, since surfaceObject contains all of these functions but we might want to make wrappers for them like this
