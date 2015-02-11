c8d06f42ae6eb45df2a5c3bd8c98d35aefc1157a
Adrian Toncean <adrian.toncean@gooengine.com>
2015-02-11 11:57:35 +0100

    FontGenerator works

---
+++ src/goo/addons/textpack/FontGenerator.js
@@ -0,0 +1,234 @@
+define(function () {
+	'use strict';
+
+	// ---
+	var con2d;
+
+	function drawPoint(x, y) {
+		con2d.fillRect(x - 1, y - 1, 3, 3);
+	}
+
+	function drawPath(points) {
+		con2d.beginPath();
+		con2d.moveTo(points[0].x, points[0].y);
+
+		con2d.fillStyle = 'black';
+		con2d.fillText(0, points[0].x, points[0].y);
+		for (var i = 1; i < points.length; i++) {
+			con2d.lineTo(points[i].x, points[i].y);
+			con2d.fillText(i, points[i].x, points[i].y);
+		}
+
+		con2d.fillStyle = 'rgba(0, 0, 255, 0.3)';
+		con2d.strokeStyle = 'red';
+		con2d.stroke();
+		con2d.fill();
+	}
+	// ---
+
+	function serializeCommand(command) {
+		// horrible code follows
+		var str = command.type;
+
+		if (command.x2 !== undefined) { str += ' ' + command.x2; }
+		if (command.y2 !== undefined) { str += ' ' + command.y2; }
+
+		if (command.x1 !== undefined) { str += ' ' + command.x1; }
+		if (command.y1 !== undefined) { str += ' ' + command.y1; }
+
+		if (command.x !== undefined) { str += ' ' + command.x; }
+		if (command.y !== undefined) { str += ' ' + command.y; }
+
+		return str;
+	}
+
+	function getPathPoints(stringifiedPath, stepLength) {
+		var svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+		svgPath.setAttribute('d', stringifiedPath);
+
+		var pathLength = svgPath.getTotalLength();
+
+		var points = [];
+		for (var i = 0; i < pathLength; i += stepLength) {
+			var point = svgPath.getPointAtLength(i);
+			points.push({ x: point.x, y: point.y });
+		}
+
+		return points;
+	}
+
+	function distance(point1, point2) {
+		return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2));
+	}
+
+	/**
+	 * Group points by the distance between them
+	 * @param {{ x: number, y: number}[]} points
+	 * @param {number} stepLength
+	 * @returns {Array}
+	 */
+	function groupPoints(points, stepLength) {
+		var groups = [];
+
+		var group = [];
+		group.push(points[0]);
+		for (var i = 1; i < points.length; i++) {
+			var p1 = points[i - 1];
+			var p2 = points[i];
+
+			var latestDistance = distance(p1, p2);
+
+			if (latestDistance > (stepLength + 0.1)) {
+				groups.push(group);
+				group = [];
+			}
+
+			group.push(p2);
+		}
+
+		groups.push(group);
+		return groups;
+	}
+
+	function isClockwise(points) {
+		var sum = 0;
+
+		for (var i = 1; i < points.length; i++) {
+			var p1 = points[i - 1];
+			var p2 = points[i];
+
+			sum += (p2.x - p1.x) * (p2.y - p1.y);
+		}
+
+		return sum >= 0;
+	}
+
+	/**
+	 * Finds the closest points of two polygons
+	 * @param polygon1
+	 * @param polygon2
+	 * @returns {{index1: number, index2: number}}
+	 */
+	function findClosestPoints(polygon1, polygon2) {
+		var minDist = Infinity;
+		var minIndex1 = -1;
+		var minIndex2 = -1;
+
+		for (var i = 0; i < polygon1.length; i++) {
+			for (var j = 0; j < polygon2.length; j++) {
+				var candidate = distance(polygon1[i], polygon2[j]);
+				if (candidate < minDist) {
+					minDist = candidate;
+					minIndex1 = i;
+					minIndex2 = j;
+				}
+			}
+		}
+
+		return { index1: minIndex1, index2: minIndex2 };
+	}
+
+	/**
+	 * Rotate an array a supplied amount of positions
+	 * @param array
+	 * @param positions
+	 * @returns {Array}
+	 */
+	function rotate(array, positions) {
+		var rotated = [];
+		for (var i = 0; i < array.length; i++) {
+			var newIndex = (i + positions) % array.length;
+			rotated.push(array[newIndex]);
+		}
+		return rotated;
+	}
+
+	var WIGGLE_EPSILON = 0.00001;
+	function wiggle(point, towards) {
+		var dx = point.x - towards.x;
+		var dy = point.y - towards.y;
+		return {
+			x: point.x + dx * WIGGLE_EPSILON,
+			y: point.y + dy * WIGGLE_EPSILON
+		};
+	}
+
+	/**
+	 * Concatenate 2 polygons in one and duplicate the ends of the arrays
+	 * @param polygon1
+	 * @param polygon2
+	 * @returns {string}
+	 */
+	function specialConcat(polygon1, polygon2) {
+		var wiggled1 = wiggle(polygon1[0], polygon1[1]);
+		var wiggled2 = wiggle(polygon2[0], polygon2[1]);
+		return polygon1.concat(wiggled1).concat(polygon2).concat(wiggled2);
+	}
+
+	/**
+	 * Merge 2 polygons to get a new polygon
+	 * @param polygon1
+	 * @param polygon2
+	 * @returns {string}
+	 */
+	function mergePolygons(polygon1, polygon2) {
+		// this find closest approach is good only for neatly placed points (like we have)
+		// you can fabricate a set of points for which the minimal distance will not yield a cut-free joining-line
+		var closestPoints = findClosestPoints(polygon1, polygon2);
+
+		var rotated1 = rotate(polygon1, closestPoints.index1);
+		var rotated2 = rotate(polygon2, closestPoints.index2);
+
+		var clockwise1 = isClockwise(rotated1);
+		var clockwise2 = isClockwise(rotated2);
+
+		if (!(clockwise1 ^ clockwise2)) {
+			polygon2.reverse();
+		}
+
+		drawPath(rotated2); //
+		drawPath(rotated1); //
+
+		return specialConcat(rotated1, rotated2);
+	}
+
+	function meshFromGlyph(glyph, fontSize) {
+		var stepLength = 20;
+
+		var path = glyph.getPath(0, 0, fontSize);
+		var stringifiedPath = path.commands.map(serializeCommand).reduce(function (prev, cur) {
+			return prev + cur;
+		}, '');
+
+		var points = getPathPoints(stringifiedPath, stepLength);
+
+		var pointGroups = groupPoints(points, stepLength);
+
+		// ---
+		pointGroups.forEach(function (group, index) {
+			con2d.fillStyle = 'rgb(' + (index * 30) + ', ' + (index * 80) + ', ' + (255 - index * 80) + ')';
+			group.forEach(function (point) {
+				drawPoint(point.x, point.y);
+			});
+			drawPath(group);
+		});
+		// ---
+
+		var surface = pointGroups[0];
+
+		for (var i = 1; i < pointGroups.length; i++) {
+			con2d.translate(250, 0); //
+			surface = mergePolygons(surface, pointGroups[i]);
+		}
+
+		return {
+			surface: surface,
+			contours: pointGroups
+		};
+	}
+
+	return {
+		meshFromGlyph: meshFromGlyph,
+		setCon2d: function (_con2d) { con2d = _con2d; }
+	};
+});
