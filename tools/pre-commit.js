dae4f7d24bd957d767b7761091c181786656c3ce
Adrian Toncean <adrian.toncean@gooengine.com>
2015-02-13 15:39:10 +0100

    Refactored pre-commit hook

--- tools/pre-commit.js
+++ tools/pre-commit.js
@@ -10,81 +10,86 @@ function fail(message) {
 	process.exit(1);
 }
 
-function sequence(thunks) {
-	var index = -1;
+function getModifiedFiles(callback) {
+	exec('git diff --staged --name-status', function (error, stdout, stderr) {
+		if (error) {
+			fail(stderr + '\nCould not get list of modified files: ' + error);
+		}
 
-	function continuation(value) {
-		if (index > thunks.length) { return; }
-		thunks[index](continuation);
-	}
+		var expression = /^[MA]\s+([\w\\\/-]+\.js)$/gm;
+		var files = [];
+		var match;
 
-	continuation();
+		while (match = expression.exec(stdout)) {
+			files.push(match[1]);
+		}
+		if (files.length === 0) {
+			process.exit(0);
+		}
+
+		callback(files);
+	});
 }
 
-// pyramid of doom
+function runJSHint(files) {
+	var command = path.resolve('./node_modules/.bin/jshint');
+	var child1 = exec(command + ' --reporter=tools/jshint-reporter.js ' + files.join(' '));
+
+	child1.stdout.on('data', function (data) {
+		process.stdout.write(data);
+	});
+	child1.stderr.on('data', function (data) {
+		process.stderr.write(data);
+	});
+
+	child1.on('exit', function (code) {
+		if (code !== 0) {
+			fail(
+				'Style check failed (see the above output).\n' +
+				'If you still wish to commit your code, run git commit -n to skip this check.\n'
+			);
+		} else {
+			process.exit(0);
+		}
+	});
+}
+
+function readdModifiedFiles(files, callback) {
+	exec('git add ' + files.join(' '), function (error, stdout, stderr) {
+		callback(files);
+	});
+}
+
 exec('node ./tools/cycleDetector.js', function (error, stdout, stderr) {
 	process.stdout.write(stdout + '\n');
 
-	if (error) {
-		fail('Fix the cycle before committing!\n');
-	} else {
-		//exec('git stash', function (error, stdout, stderr) {
-		//	var oldAverageLength = getAverageLength();
+	if (error) { fail('Fix the cycle before committing!\n'); }
 
-			//exec('git stash pop', function (error, stdout, stderr) {
-			//	var currentAverageLength = getAverageLength();
+	getModifiedFiles(function (files) {
+		exec('git stash', function (error, stdout, stderr) {
+			var oldAverageLength = getAverageLength();
 
-				//var delta = currentAverageLength - oldAverageLength;
-				//var ratio = delta / oldAverageLength;
-				//var percent = (ratio * 100).toFixed(2);
+			exec('git stash pop', function (error, stdout, stderr) {
+				var currentAverageLength = getAverageLength();
 
-				//console.log(
-				//	'You have ' + (delta > 0 ? 'increased' : 'decreased') +
-				//	' the average function length of the engine by ' + Math.abs(percent) + '%\n'
-				//);
+				var delta = currentAverageLength - oldAverageLength;
+				var ratio = delta / oldAverageLength;
+				var percent = (ratio * 100).toFixed(2);
 
-				//if (delta > 0) {
-				//	fail('Consider refactoring your code before committing!\n');
-				//} else {
-					exec('git diff --staged --name-status', function (error, stdout, stderr) {
-						if (error) {
-							fail(stderr + '\nCould not get list of modified files: ' + error);
-						}
+				console.log(
+					'You have ' + (delta > 0 ? 'increased' : 'decreased') +
+					' the average function length of the engine by ' + Math.abs(percent) + '%\n'
+				);
 
-						var expression = /^[MA]\s+([\w\\\/-]+\.js)$/gm;
-						var files = [];
-						var match;
+				if (delta > 0) {
+					fail(
+						'Consider refactoring your code before committing!\n' +
+						'If you still wish to commit your code, run git commit -n to skip this check.\n'
+					);
+				}
 
-						while (match = expression.exec(stdout)) {
-							files.push(match[1]);
-						}
-						if (files.length === 0) {
-							process.exit(0);
-						}
-
-						var command = path.resolve('./node_modules/.bin/jshint');
-						var child1 = exec(command + ' --reporter=tools/jshint-reporter.js ' + files.join(' '));
-
-						child1.stdout.on('data', function (data) {
-							process.stdout.write(data);
-						});
-						child1.stderr.on('data', function (data) {
-							process.stderr.write(data);
-						});
-
-						child1.on('exit', function (code) {
-							if (code !== 0) {
-								fail(
-									'Style check failed (see the above output).\n' +
-									'If you still wish to commit your code, run git commit -n to skip this check.\n'
-								);
-							} else {
-								process.exit(0);
-							}
-						});
-					});
-				//}
-			//});
-		//});
-	}
+				readdModifiedFiles(files, runJSHint);
+			});
+		});
+	});
 });
