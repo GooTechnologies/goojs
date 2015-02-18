e92867a8b3aa867ebfcbd0d33c2e77f5179d110e
Adrian Toncean <adrian.toncean@gooengine.com>
2015-02-18 14:09:42 +0100

    TextMeshGenerator is part of the geometrypack; moved files around; cleaned-up the visual test

---
+++ visual-test/goo/geometrypack/TextMeshGenerator/TextMeshGenerator-vtest.js
@@ -0,0 +1,42 @@
+require([
+	'goo/math/Vector3',
+	'goo/geometrypack/TextMeshGenerator',
+	'lib/V'
+], function (
+	Vector3,
+	TextMeshGenerator,
+	V
+) {
+	'use strict';
+
+	V.describe('The text mesh is generated based on a font-file');
+
+	var goo = V.initGoo();
+	var world = goo.world;
+
+	V.addOrbitCamera(new Vector3(90, Math.PI / 2, 0));
+	V.addLights();
+
+	opentype.load('Roboto-Black.ttf', function (err, font) {
+		if (err) { throw err; }
+
+		window.letter = function (text) {
+			// remove previous text
+			world.by.tag('text').forEach(function (entity) { entity.removeFromWorld(); });
+
+			var material = V.getColoredMaterial();
+			material.cullState.enabled = false;
+
+			var meshDatas = TextMeshGenerator.meshesForText(text, font);
+			meshDatas.forEach(function (meshData) {
+				world.createEntity(meshData, material, [-70, -16, 0])
+					.setTag('text')
+					.addToWorld();
+			});
+		};
+
+		letter('cookie');
+	});
+
+	V.process();
+});
