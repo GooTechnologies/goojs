fa86eab33deaa2eaac1790c0168afadcfcd0ae50
makkedude <makke01@hotmail.com>
2015-03-04 15:03:41 +0100

    add unit-tests for the LineRenderer

--- test/unit/addons/linerenderpack/LineRenderer-test.js
+++ test/unit/addons/linerenderpack/LineRenderer-test.js
@@ -16,9 +16,47 @@ define([
 		});
 
 		it('can construct', function () {
-			lineRenderer = new LineRenderer(world, new Vector3(1, 1, 1));
+			lineRenderer = new LineRenderer(world, Vector3.ONE);
 
 			expect(lineRenderer).toBeDefined();
+		});
+
+		it('can add line', function () {
+			lineRenderer = new LineRenderer(world, Vector3.ONE);
+
+			lineRenderer._addLine(Vector3.ZERO, Vector3.ONE);
+
+			//expect _numRenderingLines to have incremented
+			expect(lineRenderer._numRenderingLines).toBe(1);
+		});
+
+		it('can add to renderList', function () {
+			lineRenderer = new LineRenderer(world, Vector3.ONE);
+			var renderList = [];
+
+			lineRenderer._addLine(Vector3.ZERO, Vector3.ONE);
+
+			lineRenderer._manageRenderList(renderList);
+
+			//expect an element in the renderList array
+			expect(renderList.length).toBe(1);
+		});
+
+		it('can remove from renderList', function () {
+			lineRenderer = new LineRenderer(world, Vector3.ONE);
+			var renderList = [];
+
+			lineRenderer._addLine(Vector3.ZERO, Vector3.ONE);
+
+			//simulate two frames
+			for (var i = 0; i < 2; i++) {
+				lineRenderer._updateVertexData();
+				lineRenderer._manageRenderList(renderList);
+				lineRenderer._clear();
+			}
+
+			//expect no elements in the renderList array
+			expect(renderList.length).toBe(0);
 		});
 
 	});
