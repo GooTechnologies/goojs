f41bda530fdaef0c050beae11233c7ac201c5c14
makkedude <makke01@hotmail.com>
2015-03-04 12:23:47 +0100

    add empty unit-test files for the linerenderpack

---
+++ test/unit/addons/linerenderpack/LineRenderSystem-test.js
@@ -0,0 +1,29 @@
+define([
+	'goo/entities/World',
+	'goo/math/Vector3',
+	'goo/addons/linerenderpack/LineRenderer',
+	'goo/addons/linerenderpack/LineRenderSystem'
+], function (
+	World,
+	Vector3,
+	LineRenderer,
+	LineRenderSystem
+) {
+	'use strict';
+
+	describe('LineRenderSystem', function () {
+		var world;
+		var lineRenderSystem;
+
+		beforeEach(function () {
+			world = new World();
+		});
+
+		it('can construct', function () {
+			lineRenderSystem = new LineRenderSystem(world);
+
+			expect(lineRenderSystem).toBeDefined();
+		});
+
+	});
+});
---
+++ test/unit/addons/linerenderpack/LineRenderer-test.js
@@ -0,0 +1,27 @@
+define([
+	'goo/entities/World',
+	'goo/math/Vector3',
+	'goo/addons/linerenderpack/LineRenderer'
+], function (
+	World,
+	Vector3,
+	LineRenderer
+) {
+	'use strict';
+
+	describe('LineRenderer', function () {
+		var world;
+		var lineRenderer;
+
+		beforeEach(function () {
+			world = new World();
+		});
+
+		it('can construct', function () {
+			lineRenderer = new LineRenderer(world, new Vector3(1, 1, 1));
+
+			expect(lineRenderer).toBeDefined();
+		});
+
+	});
+});
