0637a4b75f3039aec1b1b6757cc3187c690c4714
makkedude <makke01@hotmail.com>
2015-03-05 15:38:22 +0100

    add unit-tests for encodeColor method to make sure colors are properly encoded

--- test/unit/addons/linerenderpack/LineRenderSystem-test.js
+++ test/unit/addons/linerenderpack/LineRenderSystem-test.js
@@ -15,16 +15,51 @@ define([
 
 		beforeEach(function () {
 			world = new World();
+			lineRenderSystem = new LineRenderSystem(world);
 		});
 
 		it('can construct', function () {
-			lineRenderSystem = new LineRenderSystem(world);
-
 			expect(lineRenderSystem).toBeDefined();
 		});
 
+		describe('encodeColor', function () {
+			it('can encode white', function () {
+
+				var encodedColor = lineRenderSystem.encodeColor(lineRenderSystem.WHITE);
+
+				expect(encodedColor).toBe(16777215);
+			});
+
+			it('can encode red', function () {
+
+				var encodedColor = lineRenderSystem.encodeColor(lineRenderSystem.RED);
+
+				expect(encodedColor).toBe(16711680);
+			});
+
+			it('can encode green', function () {
+
+				var encodedColor = lineRenderSystem.encodeColor(lineRenderSystem.GREEN);
+
+				expect(encodedColor).toBe(65280);
+			});
+
+			it('can encode blue', function () {
+
+				var encodedColor = lineRenderSystem.encodeColor(lineRenderSystem.BLUE);
+
+				expect(encodedColor).toBe(255);
+			});
+
+			it('can encode black', function () {
+
+				var encodedColor = lineRenderSystem.encodeColor(lineRenderSystem.BLACK);
+
+				expect(encodedColor).toBe(0);
+			});
+		});
+
 		it('can drawLine', function () {
-			lineRenderSystem = new LineRenderSystem(world);
 
 			//draw a red line between 0,0,0 and 1,1,1
 			lineRenderSystem.drawLine(Vector3.ZERO, Vector3.ONE, Vector3.UNIT_X);
@@ -33,7 +68,6 @@ define([
 		});
 
 		it('can drawCross', function () {
-			lineRenderSystem = new LineRenderSystem(world);
 
 			//draw a red cross at 0,0,0
 			lineRenderSystem.drawCross(Vector3.ZERO, Vector3.UNIT_X);
