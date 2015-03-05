867e428311c9952536a0dea944bf6f0494b8df8a
rherlitz <rikard@gootechnologies.com>
2015-03-05 19:10:48 +0100

    Updated to more common event model, and renamed to EventTarget

---
+++ src/goo/util/EventTarget.js
@@ -0,0 +1,96 @@
+define(function() {
+	'use strict';
+
+	/**
+	 * EventTarget is an interface implemented by objects that can receive events and may have listeners for them.
+	 * @example
+	 * function Test() {
+	 * 		EventTarget.apply(this, arguments);
+	 * }
+	 * Test.prototype = Object.create(EventTarget.prototype);
+	 */
+	function EventTarget() {
+		this._listenerMap = new Map();
+	}
+
+	/**
+	 * Sends an event to all listeners
+	 * @param {Event} event Event passed to the listeners
+	 * @returns {EventTarget} Self for chaining.
+	 */
+	EventTarget.prototype.fire = function (event) {
+		var listeners = this._listenerMap.get(event.type);
+		if (listeners) {
+			event.target = this;
+
+			var listenersCopy = listeners.slice();
+			for (var i = 0, l = listenersCopy.length; i < l; i++) {
+				listenersCopy[i].call(this, event);
+			}
+		}
+
+		return this;
+	};
+
+	/**
+	 * Registers a new listener for a certain event type
+	 * @param {string} type Type of event listener to add
+	 * @param {function} listener Listener to add
+	 * @returns {EventTarget} Self for chaining.
+	 */
+	EventTarget.prototype.on = function (type, listener) {
+		var listeners = this._listenerMap.get(type);
+		if (!listeners) {
+			listeners = [listener];
+			this._listenerMap.set(type, listeners);
+		} else if (listeners.indexOf(listener) === -1) {
+			listeners.push(listener);
+		}
+
+		return this;
+	};
+
+	/**
+	 * Removes a listener for a certain event type
+	 * @param {string} type Type of event listener to remove
+	 * @param {function} listener Listener to remove
+	 * @returns {EventTarget} Self for chaining.
+	 */
+	EventTarget.prototype.off = function (type, listener) {
+		if (listener) {
+			var index;
+			var listeners = this._listenerMap.get(type);
+			if (listeners && (index = listeners.indexOf(listener)) !== -1) {
+				listeners.splice(index, 1);
+
+				if (listeners.length === 0) {
+					this._listenerMap.delete(type);
+				}
+			}
+		} else {
+			this._listenerMap.delete(type);
+		}
+
+		return this;
+	};
+
+	/**
+	 * Test if there are any listeners bound to a certain event type
+	 * @param {string} type Type of event to test for
+	 * @return {boolean} If there are any listeners of specified type on this target
+	 */
+	EventTarget.prototype.has = function (type) {
+		return this._listenerMap.has(type);
+	};
+
+	/**
+	 * Get all listeners for a certain type
+	 * @param {string} type Type of event to get listeners for
+	 * @return {} All listeners of specified type attached to this target
+	 */
+	EventTarget.prototype.get = function (type) {
+		return this._listenerMap.get(type);
+	};
+
+	return EventTarget;
+});
