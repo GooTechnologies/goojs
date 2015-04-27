591b4fde881a8971a008c59e06111dacefb3ca70
Flonka <flonka@gooengine.com>
2015-04-27 20:47:34 +0300

    Something in the scene.

--- visual-test/goo/animationpack/Animation-vtest.js
+++ visual-test/goo/animationpack/Animation-vtest.js
@@ -1,13 +1,30 @@
 require([
-	'lib/V'
+	'lib/V',
+	'goo/renderer/Material',
+	'goo/renderer/shaders/ShaderLib',
+	'goo/shapes/Box',
+	'goo/math/Vector3'
 ], function (
-	V
+	V,
+	Material,
+	ShaderLib,
+	Box,
+	Vector3
 ) {
 	'use strict';
 
 	V.describe('Skeleton Animation Test');
 
 	var goo = V.initGoo();
+
+	var world = goo.world;
+
+	var boxEntity = world.createEntity(new Box(), new Material(ShaderLib.uber));
+	boxEntity.addToWorld();
+
+	V.addLights();
+
+	V.addOrbitCamera(new Vector3(15, Math.PI / 2, 0.3));
 
 	V.process();
 });
