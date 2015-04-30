bf174765f99000a2e7f1f5e2c5e4bb57be695a4f
Flonka <flonka@gootechnologies.com>
2015-04-30 14:33:27 +0300

    Moving movn.

--- visual-test/goo/animationpack/Animation-vtest.js
+++ visual-test/goo/animationpack/Animation-vtest.js
@@ -90,7 +90,7 @@ require([
 		j2._index = 0;
 		j2._parentIndex = Joint.NO_PARENT;
 
-		var j1 = new Joint('rootjoint');
+		var j1 = new Joint('asdfsfd');
 		j1._index = 1;
 		j1._parentIndex = j2._index;
 
@@ -194,18 +194,115 @@ require([
 		boxEntity.addToWorld();
 	}
 
+	function createJointChannel(joint, times, t, r, s, blendType) {
+		return new JointChannel(
+			joint._index,
+			joint._name,
+			times,
+			r,
+			t,
+			s,
+			blendType
+		);
+	}
+
 	function addFoldingPaper(world) {
 
-		// Create skeleton joint hierarchy
-		var rootJoint = new Joint()
+		var joints = [];
 
+		// Create skeleton joint hierarchy
+		var rootJoint = new Joint('RootJoint');
+		rootJoint._parentIndex = Joint.NO_PARENT;
+		rootJoint._index = 0;
+		joints.push(rootJoint);
+
+		var leftSideJoint = new Joint('Ls');
+		leftSideJoint._index = 1;
+		leftSideJoint._parentIndex = rootJoint._index
+		joints.push(leftSideJoint);
+
+		var skeleton = new Skeleton('PaperSkeleton', joints);
+		var skeletonPose = new SkeletonPose(skeleton);
+		var animComp = new AnimationComponent(skeletonPose);
+		
+		var times = [0.0, 1.0, 2.0];
+		var rots = [];
+		var q1 = new Quaternion();
+		var q2 = new Quaternion();
+		Array.prototype.push.apply(rots, q1.data);
+		Array.prototype.push.apply(rots, q1.data);
+		Array.prototype.push.apply(rots, q1.data);
+
+		var trans = [
+			0,0,0,
+			0,1,0,
+			0,0,0
+		];
+
+		var scales = [
+			1,1,1,
+			1,1,1,
+			1,1,1,
+		];
+
+		var rootChannel = createJointChannel(rootJoint, times, trans, rots, scales, 'SCurve3');
+
+		rots = [];
+		q2.fromAngleNormalAxis(Math.PI * 0.5, Vector3.UNIT_Y);
+		Array.prototype.push.apply(rots, q1.data);
+		Array.prototype.push.apply(rots, q2.data);
+		Array.prototype.push.apply(rots, q1.data);
+
+		var trans = [
+			0,0,0,
+			0,0,0,
+			0,0,0
+		];
+
+		var scales = [
+			1,1,1,
+			1,1,1,
+			1,1,1,
+		];
+
+		var leftChannel = createJointChannel(leftSideJoint, times, trans, rots, scales, 'SCurve3');
+
+		var animChannels = [rootChannel, leftChannel];
+		var clip = new AnimationClip('My animation Clip', animChannels);
+		var clipSource = new ClipSource(clip);
+		clipSource._clipInstance._loopCount = -1;  // -1 for looping infinetly
+		var animState = new SteadyState('My animation state');
+		animState.setClipSource(clipSource);
+		var animLayer = animComp.layers[0];  // Default animation layer
+		animLayer.setState('RootRotateState', animState);
+		animLayer.setCurrentState(animState, true);
 
 		var size = 10;
 		var vertCount = 100;
 		var meshData = Surface.createTessellatedFlat(size, size, vertCount, vertCount);
+
+		addSkeltonAttributeData(meshData);
+		addPaletteMap(meshData);
+		meshData.paletteMap = [0, 1];
+
+		var weightData = meshData.dataViews.WEIGHTS;
+		for (var i = 0; i < weightData.length; i+=4) {
+			weightData[i] = 1;
+			weightData[i+1] = 0;
+			weightData[i+2] = 0;
+			weightData[i+3] = 0;
+		}
+
+		var jointData = meshData.dataViews.JOINTIDS;
+		for (var i = 0; i < jointData.length; i+=4) {
+			jointData[i] = 1;
+		}
+
 		var material = new Material(ShaderLib.uber);
 		material.cullState.enabled = false;
 		var surfaceEntity = world.createEntity(meshData, material);
+		surfaceEntity.set(animComp);
+		surfaceEntity.meshDataComponent.currentPose = surfaceEntity.animationComponent._skeletonPose;
 		surfaceEntity.addToWorld();
 	}
 	
