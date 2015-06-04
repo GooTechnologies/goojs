bdc9f3a9ffce0e418fbbc57faf29d233dfd99a80
Flonka <flonka@gootechnologies.com>
2015-06-04 12:05:28 +0300

    Wait, that wasnt a Surface test. its PolyLine.

--- visual-test/goo/geometrypack/Surface/PolyLine-vtest.js
+++ visual-test/goo/geometrypack/Surface/PolyLine-vtest.js
@@ -3,14 +3,12 @@ require([
 	'goo/renderer/shaders/ShaderLib',
 	'goo/math/Vector3',
 	'goo/geometrypack/PolyLine',
-	'goo/renderer/TextureCreator',
 	'lib/V'
 ], function (
 	Material,
 	ShaderLib,
 	Vector3,
 	PolyLine,
-	TextureCreator,
 	V
 	) {
 	'use strict';
@@ -58,9 +56,6 @@ require([
 
 	// surface material
 	var surfaceMaterial = new Material(ShaderLib.texturedLit);
-	new TextureCreator().loadTexture2D('../../../resources/goo.png').then(function (texture) {
-		surfaceMaterial.setTexture('DIFFUSE_MAP', texture);
-	});
 
 	// surface entity
 	world.createEntity(surfaceMeshData, surfaceMaterial, [0, -1, 0]).addToWorld();
