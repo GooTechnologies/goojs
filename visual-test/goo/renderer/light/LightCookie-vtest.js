f32891dbb58f1e790ad14ff1d9e0f2d6470dbbdd
Flonka <flonka@gootechnologies.com>
2015-06-05 11:55:49 +0300

    Tweakiiin.

--- visual-test/goo/renderer/light/LightCookie-vtest.js
+++ visual-test/goo/renderer/light/LightCookie-vtest.js
@@ -30,38 +30,39 @@ require([
 	function addDirectionalLight() {
 		var directionalLight = new DirectionalLight(new Vector3(1, 1, 1));
 		directionalLight.intensity = 0.5;
-		var directionalEntity = goo.world.createEntity(directionalLight, [0, 0, 5]).addToWorld();
+		var directionalEntity = goo.world.createEntity(directionalLight, [50, 0, 100]).addToWorld();
 		new TextureCreator().loadTexture2D('../../../resources/goo.png').then(function (texture) {
 			directionalLight.lightCookie = texture;
 		});
+
 		return directionalEntity;
 	}
 
 	function addSpotLight() {
 		var spotLight = new SpotLight(new Vector3(1, 1, 1));
 		spotLight.angle = 45;
-		spotLight.range = 100;
+		spotLight.range = 400;
 		spotLight.penumbra = 5;
 		
-		new TextureCreator().loadTexture2D('../../../resources/Pot_Diffuse.dds').then(function (texture) {
+		new TextureCreator().loadTexture2D('../../../resources/check.png').then(function (texture) {
 			spotLight.lightCookie = texture;
 		});
 
-		var spotEntity = goo.world.createEntity('spotLight', spotLight, [0, 0, 7]);
+		var spotEntity = goo.world.createEntity('spotLight', spotLight, [0, 0, 40]);
 		
 		spotEntity.set(new ScriptComponent({
 			run: function(entity, tpf) {
 				var rot = Math.PI * 0.2 * tpf;
 				lightRotationAngle += rot;
-				var ypan = Math.sin(entity._world.time) * 0.3;
-				var xpan = Math.cos(entity._world.time) * 0.1;
+				var ypan = Math.sin(entity._world.time) * 0.45;
+				var xpan = Math.cos(entity._world.time) * 0.25;
 				targetPos.setDirect(xpan, ypan, 1);
 				targetPos.normalize();
 				rotQuat.fromAngleNormalAxis(lightRotationAngle, targetPos);
 
-				var rotMat = spotEntity.transformComponent.transform.rotation;
+				var rotMat = entity.transformComponent.transform.rotation;
 				rotQuat.toRotationMatrix(rotMat);
-				spotEntity.transformComponent._dirty = true;
+				entity.transformComponent._dirty = true;
 			
 				if (lightRotationAngle >= Math.PI * 2) {
 					lightRotationAngle = Math.PI * 2 - lightRotationAngle;
@@ -80,17 +81,17 @@ require([
 	goo.renderSystems.push(debugRenderSystem);
 	world.setSystem(debugRenderSystem);
 
-	//addDirectionalLight();
+	addDirectionalLight();
 	addSpotLight();
 
 	// Backdrop
 	var quadSize = 1000;
 	world.createEntity(new Quad(quadSize, quadSize), V.getColoredMaterial(1,1,1,1), [0, 0, -50]).addToWorld();
 
-	var spheres = V.addSpheres(5).toArray();
+	//var spheres = V.addSpheres(5).toArray();
 
 	// camera
-	V.addOrbitCamera([20, Math.PI/2, 0]);
+	V.addOrbitCamera([300, Math.PI/2, 0]);
 
 	V.process();
 });
