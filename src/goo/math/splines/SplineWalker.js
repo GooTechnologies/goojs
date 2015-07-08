b2524e7e89213b8ae0d9138c6c4c503756c4416e
Stefan Hedman <schteppe@gmail.com>
2015-07-08 13:34:09 +0200

    interpolation and looping in the SplineWalker

--- src/goo/math/splines/SplineWalker.js
+++ src/goo/math/splines/SplineWalker.js
@@ -1,7 +1,9 @@
 define([
-	'goo/math/splines/Spline'
+	'goo/math/splines/Spline',
+	'goo/math/MathUtils'
 ], function (
-	Spline
+	Spline,
+	MathUtils
 ) {
 	'use strict';
 
@@ -9,15 +11,22 @@ define([
 	 * Provides a way to interpolate on a spline with constant speed
 	 * @param {Spline} spline Spline to interpolate across
 	 * @param {number} [substepSize=0.01] substepSize The size of the substep used to approximate movement across the spline.
+	 * @param {number} [loop=false]
 	 * Small values of this parameter lead to more substeps and better precision (at the cost of more computations).
 	 * @example-link http://code.gooengine.com/latest/visual-test/goo/math/SplineWalker/SplineWalker-vtest.html Comparison or normal interpolation vs using the SplineWalker
 	 */
-	function SplineWalker(spline, substepSize) {
-		this.substepSize = substepSize || 0.01;
+	function SplineWalker(spline, substepSize, loop) {
+		this.substepSize = substepSize !== undefined ? substepSize : 0.01;
+		this.loop = loop !== undefined ? loop : false;
+
 		this._spline = spline;
 		this._segment = 0;
 		this._localT = 0;
 		this._pointer = spline.controlPoints[0].clone();
+		this._lastPointer = spline.controlPoints[0].clone();
+		this._distance = 0;
+		this._fixedDistance = 0;
+		this._lastStepSize = -1;
 	}
 
 	/**
@@ -26,10 +35,12 @@ define([
 	 * @param {Vector} store
 	 */
 	SplineWalker.prototype._localInterpolation = function (store) {
-		var p0 = this._spline.controlPoints[this._segment * 3 + 0];
-		var p1 = this._spline.controlPoints[this._segment * 3 + 1];
-		var p2 = this._spline.controlPoints[this._segment * 3 + 2];
-		var p3 = this._spline.controlPoints[this._segment * 3 + 3];
+		var segment = this._segment;
+		var spline = this._spline;
+		var p0 = spline.controlPoints[segment * 3 + 0];
+		var p1 = spline.controlPoints[segment * 3 + 1];
+		var p2 = spline.controlPoints[segment * 3 + 2];
+		var p3 = spline.controlPoints[segment * 3 + 3];
 		Spline.cubicInterpolation(p0, p1, p2, p3, this._localT, store);
 	};
 
@@ -45,8 +56,10 @@ define([
 		if (this._localT > 1) {
 			this._localT -= 1;
 			this._segment++;
-
-			if (this._segment >= this._spline.segments) {
+			if (this.loop) {
+				this._segment = this._segment % this._spline.segments;
+			}
+			if (this._segment >= this._spline.segments && !this.loop) {
 				store.copy(this._spline.controlPoints[this._spline.controlPoints.length - 1]);
 				return;
 			}
@@ -61,12 +74,27 @@ define([
 	 * @param {Vector} store The vector to use to store the resulting position; must have the same type as the control points of the spline
 	 */
 	SplineWalker.prototype.advance = function (distance, store) {
-		var walkedSoFar = 0;
-		while (walkedSoFar < distance && this._segment < this._spline.segments) {
-			this._step(this.substepSize, store);
-			walkedSoFar += this._pointer.distance(store);
-			this._pointer.copy(store);
+
+		var pointer = this._pointer;
+		var lastPointer = this._lastPointer;
+
+		this._distance += distance;
+		this._lastStepSize  = this._lastStepSize < 0 ? distance : this._lastStepSize;
+
+		if (this.loop && this._segment >= this._spline.segments) {
+			this.restart();
 		}
+
+		while (this._fixedDistance < this._distance && this._segment < this._spline.segments) {
+			this._step(this.substepSize, store);
+			this._lastStepSize = pointer.distance(store);
+			this._fixedDistance += this._lastStepSize;
+			lastPointer.copy(pointer);
+			pointer.copy(store);
+		}
+
+		var t = MathUtils.clamp((this._fixedDistance - this._distance) / this._lastStepSize, 0, 1);
+		store.copy(pointer).lerp(lastPointer, t);
 	};
 
 	/**
@@ -74,7 +102,25 @@ define([
 	 * @returns {boolean}
 	 */
 	SplineWalker.prototype.canWalk = function () {
+		if (this.loop) {
+			return true;
+		}
 		return this._segment < this._spline.segments;
+	};
+
+	/**
+	 * Restart the spline.
+	 */
+	SplineWalker.prototype.restart = function () {
+		this._segment = 0;
+		if (this.loop) {
+			this._distance = 0;
+			this._fixedDistance = 0;
+			this._lastStepSize = -1;
+			var spline = this._spline;
+			this._pointer.copy(spline.controlPoints[0]);
+			this._lastPointer.copy(spline.controlPoints[0]);
+		}
 	};
 
 	return SplineWalker;
