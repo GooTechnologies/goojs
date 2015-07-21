e8946f3cf7071283aebf966fad3263cf5680c99c
Adrian Toncean <adrian.toncean@gooengine.com>
2015-07-21 16:11:44 +0200

    Added a type expression parser and tests - supports the google closure syntax

---
+++ tools/modoc/test/spec/type-parser/tokenizer-spec.js
@@ -0,0 +1,52 @@
+// jshint node:true
+'use strict';
+
+var tokenizer = require('../../../src/type-parser/tokenizer');
+
+describe('tokenizer', function () {
+	describe('tokenize', function () {
+		var makeToken = tokenizer._makeToken;
+		var tokenize = tokenizer.tokenize;
+
+		var makeNumber = makeToken.bind(null, 'number');
+		var makeIdentifier = makeToken.bind(null, 'identifier');
+		var makeSymbol = makeToken.bind(null, 'symbol');
+
+
+		it('chops a number', function () {
+			expect(tokenize('123')).toEqual([makeNumber('123')]);
+		});
+
+		it('chops an identifier', function () {
+			expect(tokenize('asd')).toEqual([makeIdentifier('asd')]);
+		});
+
+		it('chops an more tokens', function () {
+			expect(tokenize('asd 123 *   dsa :')).toEqual([
+				makeIdentifier('asd'),
+				makeNumber('123'),
+				makeSymbol('*'),
+				makeIdentifier('dsa'),
+				makeSymbol(':')
+			]);
+		});
+
+		it('chops a real-world string', function () {
+			// google closure compiler type expression format for a function
+			expect(tokenize('function (s: string, times: number) : string')).toEqual([
+				makeIdentifier('function'),
+				makeSymbol('('),
+				makeIdentifier('s'),
+				makeSymbol(':'),
+				makeIdentifier('string'),
+				makeSymbol(','),
+				makeIdentifier('times'),
+				makeSymbol(':'),
+				makeIdentifier('number'),
+				makeSymbol(')'),
+				makeSymbol(':'),
+				makeIdentifier('string')
+			]);
+		});
+	});
+});
