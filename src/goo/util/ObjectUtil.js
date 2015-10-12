71db435bb35585246be9238751445219043c8369
Stefan Hedman <schteppe@gmail.com>
2015-10-12 11:00:04 +0200

    initial work, not tested yet

--- src/goo/util/ObjectUtil.js
+++ src/goo/util/ObjectUtil.js
@@ -1,13 +1,38 @@
 define([
 	'goo/util/ObjectUtils'
-], function (ObjectUtils) {
+], function (
+	ObjectUtils
+) {
+	'use strict';
 
 	/**
-	 * Object-related utilities
-	 * @target-class ObjectUtil ObjectUtil constructor
-	 * @require-path goo/util/ObjectUtil
-	 * @group util
-	 * @deprecated Deprecated as of 0.14.x and scheduled for removal in 0.16.0; The class has been renamed to `goo/util/ObjectUtils`
+	 * @hidden
+	 * @deprecated 
 	 */
-	return ObjectUtils;
+	function ObjectUtil(){}
+
+	function shim(f){
+		return function(){
+			console.warn('ObjectUtil has been renamed to ObjectUtils, please use ObjectUtils instead.');
+			f.apply(this, arguments);
+		};
+	}
+
+	ObjectUtil.defaults = shim(ObjectUtils.defaults);
+	ObjectUtil.copyOptions = shim(ObjectUtils.copyOptions);
+	ObjectUtil.extend = shim(ObjectUtils.extend);
+	ObjectUtil.extend = shim(ObjectUtils.extend);
+	ObjectUtil.isObject = shim(ObjectUtils.isObject);
+	ObjectUtil.clone = shim(ObjectUtils.clone);
+	ObjectUtil.isObject = shim(ObjectUtils.isObject);
+	ObjectUtil.extend = shim(ObjectUtils.extend);
+	ObjectUtil.each = shim(ObjectUtils.each);
+	ObjectUtil.forEach = shim(ObjectUtils.forEach);
+	ObjectUtil.deepClone = shim(ObjectUtils.deepClone);
+	ObjectUtil.shallowSelectiveClone = shim(ObjectUtils.shallowSelectiveClone);
+	ObjectUtil.cloneMap = shim(ObjectUtils.cloneMap);
+	ObjectUtil.cloneSet = shim(ObjectUtils.cloneSet);
+	ObjectUtil.warnOnce = shim(ObjectUtils.warnOnce);
+
+	return ObjectUtil;
 });
