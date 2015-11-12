3d26a39d6a1226b59f47e152206cd32acf487de5
Rikard Herlitz <rikard@gooengine.com>
2015-11-12 23:49:07 +0100

    Added test for simplepartitioner

--- test/unit/renderer/SimplePartitioner-test.js
+++ test/unit/renderer/SimplePartitioner-test.js
@@ -1,1 +1,79 @@
-define(['goo/renderer/SimplePartitioner'], function (SimplePartitioner) {});
+define([
+	'goo/renderer/Camera',
+	'goo/renderer/bounds/BoundingSphere',
+	'goo/math/Vector3',
+	'goo/entities/Entity',
+	'goo/entities/components/MeshRendererComponent',
+	'goo/renderer/SimplePartitioner'
+], function (
+	Camera,
+	BoundingSphere,
+	Vector3,
+	Entity,
+	MeshRendererComponent,
+	SimplePartitioner
+	) {
+	'use strict';
+
+	describe('SimplePartitioner', function () {
+		var partitioner, camera;
+
+		beforeEach(function () {
+			partitioner = new SimplePartitioner();
+			camera = new Camera();
+		});
+
+		function createEntity(x, y, z) {
+			var entity = new Entity();
+			var mrc = new MeshRendererComponent();
+			mrc.worldBound = new BoundingSphere(new Vector3(x, y, z), 1);
+			entity.set(mrc);
+			return entity;
+		}
+
+		describe('process', function () {
+			it('can partition two entities', function () {
+				var entity1 = createEntity(0, 0, 5);
+				var entity2 = createEntity(0, 0, -5);
+				var entities = [entity1, entity2];
+				var renderList = [];
+
+				partitioner.process(camera, entities, renderList);
+
+				expect(renderList).toContain(entity2);
+				expect(renderList.length).toEqual(1);
+			});
+
+			it('can filter with hide', function () {
+				var entity = createEntity(0, 0, -5);
+				var entities = [entity];
+				var renderList = [];
+
+				partitioner.process(camera, entities, renderList);
+
+				expect(renderList).toContain(entity);
+
+				entity.meshRendererComponent.hidden = true;
+				partitioner.process(camera, entities, renderList);
+
+				expect(renderList).not.toContain(entity);
+			});
+
+			it('cullmode never', function () {
+				var entity = createEntity(0, 0, 5);
+				var entities = [entity];
+				var renderList = [];
+
+				partitioner.process(camera, entities, renderList);
+
+				expect(renderList).not.toContain(entity);
+
+				entity.meshRendererComponent.cullMode = 'Never';
+				partitioner.process(camera, entities, renderList);
+
+				expect(renderList).toContain(entity);
+			});
+		});
+	});
+});
+
