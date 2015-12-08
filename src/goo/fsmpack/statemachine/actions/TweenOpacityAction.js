1ef695cf9d404db103acac777b6c89abe5a12c20
Stefan Hedman <schteppe@gmail.com>
2015-12-08 15:15:55 +0100

    added key to tween material opacity action

--- src/goo/fsmpack/statemachine/actions/TweenOpacityAction.js
+++ src/goo/fsmpack/statemachine/actions/TweenOpacityAction.js
@@ -15,6 +15,7 @@ define([
 	TweenOpacityAction.prototype.constructor = TweenOpacityAction;
 
 	TweenOpacityAction.external = {
+		key: 'Tween Opacity',
 		name: 'Tween Material Opacity',
 		type: 'texture',
 		description: 'Tweens the opacity of a material',
