6418e6f21fb45816a2897c1de83282a6278e7e31
rherlitz <rikard@gootechnologies.com>
2016-02-02 14:25:58 +0100

    Adding timeline actions

---
+++ src/goo/fsmpack/statemachine/actions/StopTimelineAction.js
@@ -0,0 +1,33 @@
+define([
+	'goo/fsmpack/statemachine/actions/Action'
+], function (
+	Action
+) {
+	'use strict';
+
+	function StopTimelineAction(/*id, settings*/) {
+		Action.apply(this, arguments);
+	}
+
+	StopTimelineAction.prototype = Object.create(Action.prototype);
+	StopTimelineAction.prototype.constructor = StopTimelineAction;
+
+	StopTimelineAction.external = {
+		name: 'Stop Timeline',
+		type: 'fx',
+		description: 'Stops the timeline.',
+		canTransition: true,
+		parameters: [],
+		transitions: []
+	};
+
+	StopTimelineAction.prototype._run = function (fsm) {
+		var entity = fsm.getOwnerEntity();
+
+		if (!entity.hasComponent('TimelineComponent')) { return; }
+
+		entity.timelineComponent.stop();
+	};
+
+	return StopTimelineAction;
+});
