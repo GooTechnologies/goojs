ea59469d2db3d2bd44696dd823c4b9b753334da4
rherlitz <rikard@gootechnologies.com>
2016-02-10 11:44:56 +0100

    Working hover enter/exit actions. (needs sync fsm though)

--- src/goo/fsmpack/statemachine/actions/HoverEnterAction.js
+++ src/goo/fsmpack/statemachine/actions/HoverEnterAction.js
@@ -13,33 +13,39 @@ define([
 		this.everyFrame = true;
 		this.updated = false;
 		this.first = true;
-
-		this.currentEntity = null;
+		this.hit = false;
 
 		var that = this;
-		this.moveListener = function (event) {
-			if (that.first) {
-				that.first = false;
-				that.currentEntity = event.entity;
-				return;
+		var isHit = function (entity) {
+			if (!entity) {
+				return false;
 			}
-
-			if (!event.entity) {
-				that.currentEntity = null;
-				return;
-			}
-
-			event.entity.traverseUp(function (entity) {
-				if (entity === that.ownerEntity && that.currentEntity !== that.ownerEntity) {
-					that.updated = true;
+			var hit = false;
+			entity.traverseUp(function (entity) {
+				if (entity === that.ownerEntity) {
+					hit = true;
 					return false;
 				}
 			});
-
-			that.currentEntity = event.entity;
+			return hit;
 		};
 
-		this.moveListenerBounds = function (event) {
+		var checkEnter = function (entity) {
+			var hit = isHit(entity);
+
+			if (that.first) {
+				that.first = false;
+				that.hit = hit;
+				return;
+			}
+
+			if (!that.hit && hit) {
+				that.updated = true;
+			}
+			that.hit = hit;
+		};
+
+		this.moveListener = function (event) {
 			var x, y;
 			var domTarget = that.goo.renderer.domElement;
 			if (event.type === 'touchstart' || event.type === 'touchend' || event.type === 'touchmove') {
@@ -52,19 +58,19 @@ define([
 			}
 
 			var camera = that.goo.renderSystem.camera;
-			var pickResult = { entity: null };
+			var pickedEntity = null;
 
-			if (this.type === HoverEnterAction.types.slow) {
-				var pickingStore = this.pickSync(x, y);
-				pickResult.entity = this.world.entityManager.getEntityByIndex(pickingStore.id);
+			if (that.type === HoverEnterAction.types.slow) {
+				var pickingStore = that.goo.pickSync(x, y);
+				pickedEntity = that.goo.world.entityManager.getEntityByIndex(pickingStore.id);
 			} else {
 				var pickList = BoundingPicker.pick(that.goo.world, camera, x, y);
 				if (pickList.length > 0) {
-					pickResult.entity = pickList[0].entity;
+					pickedEntity = pickList[0].entity;
 				}
 			}
-			
-			that.moveListener(pickResult);
+
+			checkEnter(pickedEntity);
 		};
 	}
 
@@ -101,17 +107,12 @@ define([
 		this.ownerEntity = fsm.getOwnerEntity();
 		this.goo = this.ownerEntity._world.gooRunner;
 
-		// if (this.type === HoverEnterAction.types.slow) {
-		// 	this.goo.addEventListener('mousemove', this.moveListener);
-		// 	this.goo.addEventListener('touchmove', this.moveListener);
-		// } else {
-			document.addEventListener('mousemove', this.moveListenerBounds);
-			document.addEventListener('touchmove', this.moveListenerBounds);
-		// }
+		document.addEventListener('mousemove', this.moveListener);
+		document.addEventListener('touchmove', this.moveListener);
 		
 		this.updated = false;
 		this.first = true;
-		this.currentEntity = null;
+		this.hit = false;
 	};
 
 	HoverEnterAction.prototype._run = function (fsm) {
@@ -122,15 +123,8 @@ define([
 	};
 
 	HoverEnterAction.prototype.exit = function () {
-		if (this.goo) {
-			// if (this.type === HoverEnterAction.types.slow) {
-			// 	this.goo.removeEventListener('mousemove', this.moveListener);
-			// 	this.goo.removeEventListener('touchmove', this.moveListener);
-			// } else {
-				document.removeEventListener('mousemove', this.moveListenerBounds);
-				document.removeEventListener('touchmove', this.moveListenerBounds);
-			// }
-		}
+		document.removeEventListener('mousemove', this.moveListener);
+		document.removeEventListener('touchmove', this.moveListener);
 	};
 
 	return HoverEnterAction;
--- src/goo/fsmpack/statemachine/actions/HoverExitAction.js
+++ src/goo/fsmpack/statemachine/actions/HoverExitAction.js
@@ -13,37 +13,39 @@ define([
 		this.everyFrame = true;
 		this.updated = false;
 		this.first = true;
-
-		this.currentEntity = null;
+		this.hit = false;
 
 		var that = this;
-		this.moveListener = function (event) {
-			if (that.first) {
-				that.first = false;
-				that.currentEntity = event.entity;
-				return;
+		var isHit = function (entity) {
+			if (!entity) {
+				return false;
 			}
-
-			if (that.currentEntity === that.ownerEntity && event.entity !== that.currentEntity) {
-				this.updated = true;
-			}
-
-			if (!event.entity) {
-				that.currentEntity = null;
-				return;
-			}
-
-			// event.entity.traverseUp(function (entity) {
-			// 	if (entity === that.ownerEntity && that.currentEntity !== that.ownerEntity) {
-			// 		that.updated = true;
-			// 		return false;
-			// 	}
-			// });
-
-			that.currentEntity = event.entity;
+			var hit = false;
+			entity.traverseUp(function (entity) {
+				if (entity === that.ownerEntity) {
+					hit = true;
+					return false;
+				}
+			});
+			return hit;
 		};
 
-		this.moveListenerBounds = function (event) {
+		var checkExit = function (entity) {
+			var hit = isHit(entity);
+
+			if (that.first) {
+				that.first = false;
+				that.hit = hit;
+				return;
+			}
+
+			if (that.hit && !hit) {
+				that.updated = true;
+			}
+			that.hit = hit;
+		};
+
+		this.moveListener = function (event) {
 			var x, y;
 			var domTarget = that.goo.renderer.domElement;
 			if (event.type === 'touchstart' || event.type === 'touchend' || event.type === 'touchmove') {
@@ -56,12 +58,19 @@ define([
 			}
 
 			var camera = that.goo.renderSystem.camera;
-			var pickList = BoundingPicker.pick(that.goo.world, camera, x, y);
-			var pickResult = { entity: null };
-			if (pickList.length > 0) {
-				pickResult.entity = pickList[0].entity;
+			var pickedEntity = null;
+
+			if (that.type === HoverExitAction.types.slow) {
+				var pickingStore = that.goo.pickSync(x, y);
+				pickedEntity = that.goo.world.entityManager.getEntityByIndex(pickingStore.id);
+			} else {
+				var pickList = BoundingPicker.pick(that.goo.world, camera, x, y);
+				if (pickList.length > 0) {
+					pickedEntity = pickList[0].entity;
+				}
 			}
-			that.moveListener(pickResult);
+
+			checkExit(pickedEntity);
 		};
 	}
 
@@ -98,17 +107,12 @@ define([
 		this.ownerEntity = fsm.getOwnerEntity();
 		this.goo = this.ownerEntity._world.gooRunner;
 
-		if (this.type === HoverExitAction.types.slow) {
-			this.goo.addEventListener('mousemove', this.moveListener);
-			this.goo.addEventListener('touchmove', this.moveListener);
-		} else {
-			document.addEventListener('mousemove', this.moveListenerBounds);
-			document.addEventListener('touchmove', this.moveListenerBounds);
-		}
+		document.addEventListener('mousemove', this.moveListener);
+		document.addEventListener('touchmove', this.moveListener);
 		
 		this.updated = false;
 		this.first = true;
-		this.currentEntity = null;
+		this.hit = false;
 	};
 
 	HoverExitAction.prototype._run = function (fsm) {
@@ -119,15 +123,8 @@ define([
 	};
 
 	HoverExitAction.prototype.exit = function () {
-		if (this.goo) {
-			if (this.type === HoverExitAction.types.slow) {
-				this.goo.removeEventListener('mousemove', this.moveListener);
-				this.goo.removeEventListener('touchmove', this.moveListener);
-			} else {
-				document.removeEventListener('mousemove', this.moveListenerBounds);
-				document.removeEventListener('touchmove', this.moveListenerBounds);
-			}
-		}
+		document.removeEventListener('mousemove', this.moveListener);
+		document.removeEventListener('touchmove', this.moveListener);
 	};
 
 	return HoverExitAction;
