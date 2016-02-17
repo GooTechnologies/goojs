ae6ec6360273e07300c64c245f0d03fef3b94d1d
Stefan Hedman <schteppe@gmail.com>
2016-02-17 15:13:45 +0100

    SetClearColorAction: dont reset the clear color on cleanup

--- src/goo/fsmpack/statemachine/actions/SetClearColorAction.js
+++ src/goo/fsmpack/statemachine/actions/SetClearColorAction.js
@@ -7,7 +7,6 @@ define([
 
 	function SetClearColorAction(/*id, settings*/) {
 		Action.apply(this, arguments);
-		this._oldClearColor = [];
 	}
 
 	SetClearColorAction.prototype = Object.create(Action.prototype);
@@ -28,32 +27,10 @@ define([
 		transitions: []
 	};
 
-	SetClearColorAction.prototype.ready = function (fsm) {
-		var entity = fsm.getOwnerEntity();
-		var goo = entity._world.gooRunner;
-		this._oldClearColor[0] = goo.renderer._clearColor.x;
-		this._oldClearColor[1] = goo.renderer._clearColor.y;
-		this._oldClearColor[2] = goo.renderer._clearColor.z;
-		this._oldClearColor[3] = goo.renderer._clearColor.w;
-	};
-
 	SetClearColorAction.prototype.enter = function (fsm) {
 		var entity = fsm.getOwnerEntity();
-		var goo = entity._world.gooRunner;
-		goo.renderer.setClearColor(this.color[0], this.color[1], this.color[2], 1);
-	};
-
-	SetClearColorAction.prototype.cleanup = function (fsm) {
-		var entity = fsm.getOwnerEntity();
-		if (entity) {
-			var goo = entity._world.gooRunner;
-			goo.renderer.setClearColor(
-				this._oldClearColor[0],
-				this._oldClearColor[1],
-				this._oldClearColor[2],
-				this._oldClearColor[3]
-			);
-		}
+		var color = this.color;
+		entity._world.gooRunner.renderer.setClearColor(color[0], color[1], color[2], 1);
 	};
 
 	return SetClearColorAction;
