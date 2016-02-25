02186c904911b4fa4dd5f54cb84be1fa5eba610e
Stefan Hedman <schteppe@gmail.com>
2016-02-25 17:22:15 +0100

    better descriptions in event actions

--- src/goo/fsmpack/statemachine/actions/EmitAction.js
+++ src/goo/fsmpack/statemachine/actions/EmitAction.js
@@ -18,12 +18,12 @@ define([
 		key: 'Emit message',
 		name: 'Emit Message',
 		type: 'transitions',
-		description: 'Emits a message (a ping) to a channel on the bus. Messages can be listened to by the Listen action, or by scripts using the SystemBus.addListener(channel, callback) function.',
+		description: 'Emits a message (event) to a channel on the bus. Messages can be listened to by the Listen action, or by scripts using the SystemBus.addListener(channel, callback) function.',
 		parameters: [{
 			name: 'Channel',
 			key: 'channel',
 			type: 'string',
-			description: 'Channel to transmit a message (a ping) on',
+			description: 'Channel to transmit a message (event) on',
 			'default': ''
 		}],
 		transitions: []
--- src/goo/fsmpack/statemachine/actions/TransitionOnMessageAction.js
+++ src/goo/fsmpack/statemachine/actions/TransitionOnMessageAction.js
@@ -18,7 +18,7 @@ define([
 		key: 'Transition on Message',
 		name: 'Listen',
 		type: 'transitions',
-		description: 'Performs a transition on receiving a system bus message (a ping) on a specific channel',
+		description: 'Performs a transition on receiving a system bus message (event) on a specific channel',
 		canTransition: true,
 		parameters: [{
 			name: 'Message channel',
