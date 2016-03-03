ac81f7a8d581b5290f1d500da2400e864a531f99
Rikard Herlitz <rikard@gooengine.com>
2016-03-03 16:23:52 +0100

    Fixed the bug for the bugfix :)

--- src/goo/renderer/shaders/ShaderBuilder.js
+++ src/goo/renderer/shaders/ShaderBuilder.js
@@ -721,7 +721,7 @@ define([
 						);
 						if (useLightCookie) {
 							fragment.push(
-								'cookie = texture2D(lightCookie' + i + ', depth.xy).rgb;',
+								'vec4 cookieTex = texture2D(lightCookie' + i + ', depth.xy);',
 								'cookie = cookieTex.rgb * cookieTex.a;'
 							);
 						}
