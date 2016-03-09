8b7b026038c67299659b91d85ff4e30e6a79a105
rherlitz <rikard@gootechnologies.com>
2016-03-09 14:56:01 +0100

    Updated CHANGES

--- src/goo/fsmpack/statemachine/actions/SetClearColorAction.js
+++ src/goo/fsmpack/statemachine/actions/SetClearColorAction.js
@@ -19,10 +19,10 @@ define([
 		parameters: [{
 			name: 'Color',
 			key: 'color',
-			type: 'vec3',
+			type: 'vec4',
 			control: 'color',
 			description: 'Color.',
-			'default': [1, 1, 1]
+			'default': [1, 1, 1, 1]
 		}],
 		transitions: []
 	};
@@ -30,7 +30,7 @@ define([
 	SetClearColorAction.prototype.enter = function (fsm) {
 		var entity = fsm.getOwnerEntity();
 		var color = this.color;
-		entity._world.gooRunner.renderer.setClearColor(color[0], color[1], color[2], 1);
+		entity._world.gooRunner.renderer.setClearColor(color[0], color[1], color[2], color[3]);
 	};
 
 	return SetClearColorAction;
