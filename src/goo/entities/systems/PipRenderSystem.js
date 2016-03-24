ceb282c73b0b08e5fd74df709c02f52de6afb000
rherlitz <rikard@gootechnologies.com>
2016-03-24 15:06:17 +0100

    Reverted lock rendering

--- src/goo/entities/systems/PipRenderSystem.js
+++ src/goo/entities/systems/PipRenderSystem.js
@@ -4,10 +4,8 @@ define([
 	'goo/renderer/MeshData',
 	'goo/renderer/Shader',
 	'goo/shapes/Quad',
-	'goo/util/ObjectUtils',
 	'goo/renderer/pass/RenderTarget',
 	'goo/renderer/Material',
-	'goo/renderer/Texture',
 	'goo/renderer/shaders/ShaderLib',
 	'goo/renderer/pass/FullscreenPass',
 	'goo/renderer/pass/FullscreenUtils'
@@ -17,10 +15,8 @@ define([
 	MeshData,
 	Shader,
 	Quad,
-	ObjectUtils,
 	RenderTarget,
 	Material,
-	Texture,
 	ShaderLib,
 	FullscreenPass,
 	FullscreenUtils
@@ -56,21 +52,6 @@ define([
 			materials: [material]
 		};
 
-		this.textureLock = this.getLockTexture(true);
-		this.textureUnlock = this.getLockTexture(false);
-
-		var renderPipQuadNoEdge = ObjectUtils.deepClone(renderPipQuad);
-		renderPipQuadNoEdge.defines.EDGE = false;
-		this.materialLock = new Material(renderPipQuadNoEdge);
-		this.materialLock.blendState.blending = 'TransparencyBlending';
-		this.materialLock.setTexture('DIFFUSE_MAP', this.textureLock);
-		this.quadLock = new Quad(1, 1);
-		this.updateQuad(this.quadLock, 12, 15, 20, 20);
-		this.renderableLock = {
-			meshData: this.quadLock,
-			materials: [this.materialLock]
-		};
-
 		this.renderList = [];
 		this.usePostEffects = false;
 		this.camera = null;
@@ -92,43 +73,6 @@ define([
 	PipRenderSystem.prototype = Object.create(System.prototype);
 	PipRenderSystem.prototype.constructor = PipRenderSystem;
 
-	PipRenderSystem.prototype.getLockTexture = function (lock) {
-		var canvas = document.createElement('canvas');
-		canvas.width = 20;
-		canvas.height = 20;
-		var ctx = canvas.getContext('2d');
-
-		ctx.scale(0.2, 0.2);
-
-		ctx.translate(lock ? 0 : -18, 0);
-		ctx.strokeStyle = "rgb(30, 30, 30)";
-		ctx.lineWidth = 12;
-		ctx.beginPath();
-		ctx.arc(50, 35, 16, Math.PI, Math.PI * 2);
-		ctx.stroke();
-
-		ctx.translate(lock ? 0 : 30, 0);
-		ctx.lineWidth = 1;
-		ctx.fillStyle = "rgb(30, 30, 30)";
-		ctx.fillRect(20, 38, 60, 55);
-
-		ctx.fillStyle = "rgb(180, 180, 180)";
-		ctx.beginPath();
-		ctx.ellipse(50, 60, 7, 7, 0, 0, Math.PI * 2);
-		ctx.fill();
-		ctx.beginPath();
-		ctx.moveTo(50, 60);
-		ctx.lineTo(45, 77);
-		ctx.lineTo(55, 77);
-		ctx.fill();
-
-		var texture = new Texture(canvas, {
-			minFilter: 'NearestNeighborNoMipMaps',
-			magFilter: 'NearestNeighbor'
-		}, 20, 20);
-		return texture;
-	};	
-
 	PipRenderSystem.prototype.updateQuad = function (quad, x, y, width, height) {
 		quad.getAttributeBuffer(MeshData.POSITION).set([
 			x, y, 0,
@@ -144,6 +88,8 @@ define([
 			return;
 		}
 
+		this.renderer = renderer;
+
 		var aspect = this.camera.aspect;
 		if (aspect !== this.aspect) {
 			this.aspect = aspect;
@@ -151,7 +97,6 @@ define([
 			var width = height * aspect;
 
 			this.updateQuad(this.quad, 10, 10, width, height);
-			this.updateQuad(this.quadLock, 12, height - 15, 20, 20);
 		}
 
 		renderer.updateShadows(this.renderSystem.partitioner, this.renderSystem.entities, this.renderSystem.lights);
@@ -180,7 +125,6 @@ define([
 		}
 
 		renderer.render(this.renderableQuad, FullscreenUtils.camera, [], null, false);
-		renderer.render(this.renderableLock, FullscreenUtils.camera, [], null, false);
 	};
 
 	var renderPipQuad = {
