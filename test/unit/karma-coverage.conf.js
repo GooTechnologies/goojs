26db0e324293be340494bb2cb247bde506295a78
Stefan Hedman <schteppe@gmail.com>
2016-03-31 17:10:13 +0200

    use tabs in karma config

--- test/unit/karma-coverage.conf.js
+++ test/unit/karma-coverage.conf.js
@@ -1,81 +1,80 @@
 var path = require('path');
 var webpack = require('webpack');
 
-module.exports = function(config) {
-    config.set({
-        browsers: ['Chrome'],
-        captureTimeout: 60000,
-        browserDisconnectTimeout: 60000,
-        browserNoActivityTimeout: 60000,
+module.exports = function (config) {
+	config.set({
+		browsers: ['Chrome'],
+		captureTimeout: 60000,
+		browserDisconnectTimeout: 60000,
+		browserNoActivityTimeout: 60000,
 
-        basePath: '../../',
+		basePath: '../../',
 
-        files: [
-            'lib/cannon/cannon.min.js',
-            { pattern: 'test/unit/**/*.mp4', included: false },
-            { pattern: 'test/unit/**/*.png', included: false },
-            'test/unit/**/*-test.js'
-        ],
+		files: [
+			'lib/cannon/cannon.min.js',
+			{ pattern: 'test/unit/**/*.mp4', included: false },
+			{ pattern: 'test/unit/**/*.png', included: false },
+			'test/unit/**/*-test.js'
+		],
 
-        frameworks: ['jasmine'],
+		frameworks: ['jasmine'],
 
-        plugins: [
-            'karma-chrome-launcher',
-            'karma-jasmine',
-            'karma-sourcemap-loader',
-            'karma-webpack',
-            'karma-coverage',
-        ],
+		plugins: [
+			require('karma-coverage'),
+			require('karma-jasmine'),
+			require('karma-chrome-launcher'),
+			require('karma-webpack')
+		],
 
-        preprocessors: {
-            'test/unit/**/*-test.js': ['webpack']
-        },
+		preprocessors: {
+			'test/unit/**/*-test.js': ['webpack']
+		},
 
-        reporters: ['dots', 'coverage'],
+		reporters: ['dots', 'coverage'],
 
-        coverageReporter: {
-            reporters: [{
-                type: 'text-summary',
-            }, {
-                type: 'html',
-                dir: 'coverage'
-            }]
-        },
+		coverageReporter: {
+			reporters: [{
+				type: 'text-summary'
+			}, {
+				type: 'html',
+				dir: 'coverage'
+			}]
+		},
 
-        singleRun: true,
+		singleRun: true,
 
-        webpack: {
-            resolve: {
-                // Everything relative to repo root
-                root: path.resolve(path.join(__dirname, '..', '..'))
-            },
+		webpack: {
+			resolve: {
+				// Everything relative to repo root
+				root: path.resolve(path.join(__dirname, '..', '..'))
+			},
 
-            node: {
-                fs: 'empty'
-            },
+			node: {
+				fs: 'empty'
+			},
 
-            // Instrument code that isn't test or vendor code.
-            module: {
-                loaders: [{
-                    test: /\.js?$/,
-                    include: path.join(__dirname, 'src'),
-                    loader: 'babel?stage=0'
-                }],
-                postLoaders: [{
-                    test: /\.js$/,
-                    exclude: /(test|node_modules)\//,
-                    loader: 'istanbul-instrumenter'
-                }]
-            },
+			// Instrument code that isn't test or vendor code.
+			module: {
+				loaders: [{
+					test: /\.js?$/,
+					include: path.join(__dirname, 'src'),
+					loader: 'babel?stage=0'
+				}],
+				postLoaders: [{
+					test: /\.js$/,
+					exclude: /(test|node_modules)\//,
+					loader: 'istanbul-instrumenter'
+				}]
+			},
 
-            plugins: [
-                new webpack.ProvidePlugin(require('./karmaWebpackProvidePluginSettings'))
-            ]
+			plugins: [
+				new webpack.ProvidePlugin(require('./karmaWebpackProvidePluginSettings'))
+			]
 
-        },
+		},
 
-        webpackMiddleware: {
-            noInfo: true
-        }
-    });
+		webpackMiddleware: {
+			noInfo: true
+		}
+	});
 };
